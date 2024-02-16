5107aacdc0c746eb3b4f00cf41ea002e33f837e2
nezouse <stankiewicz1998@gmail.com>
2024-02-16 13:35:07 -0600

    🍞 Store auth data in database

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/drizzle/db";
 
 export const {
   signIn,
@@ -7,6 +9,7 @@ export const {
   auth,
   handlers: { GET, POST },
 } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
