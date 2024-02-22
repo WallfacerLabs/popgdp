ceb94772c9fa1a51897653143fd200f342a33d44
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-02-22 09:32:56 -0600

    🐝 Add createWaveSchema tests (#12)

---
+++ constants/errorMessages.ts
@@ -0,0 +1,11 @@
+export const errorMessages = {
+  required(fieldName: string) {
+    return `${fieldName} is required`;
+  },
+  minLength(fieldName: string, length: number) {
+    return `${fieldName} must be at least ${length} characters long`;
+  },
+  maxLength(fieldName: string, length: number) {
+    return `${fieldName} must be at most ${length} characters long`;
+  },
+};
