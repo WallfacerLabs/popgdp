5468292376cc2545de1ffa129f0e70e553faf8df
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-03-04 11:36:41 +0100

    👩🏻‍🚀 Update comments component (#33)

--- app/waves/[waveId]/applications/[applicationId]/page.tsx
+++ app/waves/[waveId]/applications/[applicationId]/page.tsx
@@ -5,6 +5,7 @@ import { formatDate } from "@/lib/dates";
 import { parseMarkdown } from "@/lib/parseMarkdown";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 import { AddCommentForm } from "./addCommentForm/addCommentForm";
 
@@ -53,82 +54,46 @@ export default async function Application({
                 __html: applicationHtml,
               }}
             />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Excepturi exercitationem dicta aspernatur veniam doloremque
-              tempora harum officia quo fugit assumenda. Vero ad obcaecati optio
-              ab iusto natus, quam iure ullam. Odit laboriosam maxime numquam
-              amet doloribus velit, fugit eius doloremque deleniti non inventore
-              soluta nobis deserunt similique! Molestias commodi molestiae magni
-              quisquam? Et dolorum voluptatum quod id inventore quae sapiente!
-              Temporibus cum consectetur iure eius, eaque qui rerum neque quo,
-              vitae reiciendis distinctio quas obcaecati. Maxime dolorum
-              obcaecati esse voluptatem sapiente, repudiandae dolores beatae,
-              aut autem debitis ab dolore impedit! Alias inventore repellat
-              illum! Doloribus facilis cum, consequatur blanditiis officiis
-              adipisci, a nostrum voluptatem sint minus soluta unde illo
-              voluptates, nesciunt fuga? Nostrum, quod. Voluptatum sapiente
-              molestiae id veritatis in. Ea, quae repellendus. Quia, dolorem
-              beatae natus non adipisci laudantium minus. Quos tempore nostrum
-              optio doloribus aut reprehenderit, vel beatae sequi quia molestias
-              expedita adipisci dolorum consectetur cum ducimus commodi!
-              Praesentium est rem molestias dolores sunt possimus dicta
-              laudantium sequi minus quidem saepe eos, soluta, ut dolore vero
-              libero quos recusandae cupiditate tenetur officia illum nam enim.
-              Praesentium, debitis molestias! Labore nihil optio deserunt
-              sapiente. Optio atque, sed et sequi totam rerum voluptates
-              similique est eum, soluta voluptas vero maxime excepturi vel
-              inventore modi libero tempora aperiam fuga. Error, reiciendis!
-              Quis, reprehenderit delectus amet eos quo vel minus exercitationem
-              eum dolores possimus culpa totam sint ad, molestiae cum, sunt qui
-              natus tenetur nostrum id quam sequi? Maxime quos totam rem? Earum
-              dicta ullam maxime quod dolores praesentium repellat magnam ex
-              fugiat ea quidem, hic aliquam atque vel blanditiis mollitia
-              ratione. Commodi cum reiciendis vero, neque aliquid eum magni qui
-              libero. Porro, modi. Similique, dolorum nulla accusantium
-              perspiciatis aut accusamus rerum dolor. Totam recusandae,
-              exercitationem perferendis, veniam quod accusamus ipsam enim atque
-              deserunt, aliquam reiciendis non nobis laudantium maiores fugiat
-              eaque? Vitae quasi aliquam dolore magnam dicta neque deleniti
-              aliquid? Quibusdam suscipit, est autem similique soluta laborum
-              error inventore velit aut, at sed doloremque ipsum quam! Cumque
-              quasi iure labore error. Commodi nam quam nesciunt in repellendus
-              aliquid omnis, illo dolores nihil, nemo voluptas soluta sed
-              repudiandae a, ipsam esse. Consequatur, provident. Consequuntur,
-              eaque laborum? Maxime possimus culpa quae itaque iusto?
-            </p>
           </div>
         </div>
       </Card>
 
-      {application.comments.map((comment, i) => (
-        <Card className="flex gap-8 px-6" key={comment.id}>
-          <Avatar className="my-6">
-            <AvatarFallback />
+      <Separator className="my-16" />
 
-            <AvatarImage
-              src={application.users.image || undefined}
-              alt={
-                application.users.name
-                  ? `${application.users.name} avatar`
-                  : undefined
-              }
-            />
-          </Avatar>
+      <div className="flex flex-col gap-8">
+        <div className="flex items-start gap-1">
+          <h3 className="text-xl font-bold">Comments</h3>
+          <span className="text-sm">({application.comments.length})</span>
+        </div>
 
-          <div className="my-4 w-full">
-            <div className="mb-4 flex items-baseline justify-between text-gray-600">
-              <div className="font-medium">{application.users.name}</div>
-              <div className="text-sm">{formatDate(comment.createdAt)}</div>
-            </div>
-            <div className="prose max-w-none">
-              <div dangerouslySetInnerHTML={{ __html: commentsHtml[i] }} />
+        {application.comments.map((comment, i) => (
+          <div className="flex items-center gap-4" key={comment.id}>
+            <Avatar>
+              <AvatarFallback />
+
+              <AvatarImage
+                src={application.users.image || undefined}
+                alt={
+                  application.users.name
+                    ? `${application.users.name} avatar`
+                    : undefined
+                }
+              />
+            </Avatar>
+
+            <div className="flex w-full flex-col justify-between">
+              <div
+                className="prose prose-sm max-w-none"
+                dangerouslySetInnerHTML={{ __html: commentsHtml[i] }}
+              />
+
+              <div className="text-sm font-bold">{application.users.name}</div>
             </div>
           </div>
-        </Card>
-      ))}
+        ))}
 
-      <AddCommentForm />
+        <AddCommentForm />
+      </div>
     </div>
   );
 }
