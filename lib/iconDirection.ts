15bb8ecd6d6670a0bae57df0c71ea14a47b7accc
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-04 15:51:41 +0100

    🖼️ Add icons (#38)
    Co-authored-by: Łukasz Stankiewicz <stankiewicz1998@gmail.com>

---
+++ lib/iconDirection.ts
@@ -0,0 +1,18 @@
+import type { VariantProps } from 'class-variance-authority'
+import { cva } from 'class-variance-authority'
+
+export const iconDirections = cva("origin-center", {
+  defaultVariants: {
+    direction: 'right',
+  },
+  variants: {
+    direction: {
+      up: '-rotate-90',
+      down: 'rotate-90',
+      left: 'rotate-180',
+      right: 'rotate-0',
+    },
+  },
+})
+
+export type IconDirection = VariantProps<typeof iconDirections>
