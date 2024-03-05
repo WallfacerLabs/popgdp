a69b206677b685eb5c4ec87773bf2d44ddfaefc0
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-03-05 11:16:42 +0100

    🖱️ Add steps icons (#48)
    Co-authored-by: mazya <36150652+ClumsyVlad@users.noreply.github.com>
    Co-authored-by: mazya <mazur097mazur@gmail.com>

--- app/waves/[waveId]/applications/create/stepper.tsx
+++ app/waves/[waveId]/applications/create/stepper.tsx
@@ -29,7 +29,10 @@ export function CreateApplicationStepper({
       </div>
 
       <div className="flex w-full max-w-xl flex-col gap-6">
-        <span className="font-bold">{stepsConfig[currentStep].name}</span>
+        <div className="flex items-center gap-2">
+          {stepsConfig[currentStep].icon}
+          <span className="font-bold">{stepsConfig[currentStep].name}</span>
+        </div>
         {children[currentStep]}
       </div>
     </div>
