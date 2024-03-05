1065d537b7dd5d1cfa28704cd2b282679699aeda
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-03-05 15:11:16 +0100

    🦸 Store stepsProvider state in local storage (#56)

--- app/waves/[waveId]/applications/create/page.tsx
+++ app/waves/[waveId]/applications/create/page.tsx
@@ -1,12 +1,13 @@
+import dynamic from "next/dynamic";
+
 import { BackButton } from "@/components/ui/backButton";
 
-import { CreateApplicationStepper } from "./stepper";
-import { GrantScoping } from "./steps/grantScoping";
-import { MainDetails } from "./steps/mainDetails";
-import { Resources } from "./steps/resources";
-import { Roadmap } from "./steps/roadmap";
-import { TeamInformation } from "./steps/teamInformation";
-import { StepsContextProvider } from "./stepsProvider";
+const CreateApplicationForm = dynamic(
+  () => import("./createApplicationForm").then((mod) => mod.Form),
+  {
+    ssr: false,
+  },
+);
 
 export default function CreateApplication({
   params,
@@ -20,15 +21,7 @@ export default function CreateApplication({
         <h2 className="text-2xl font-bold">Apply for the Grant</h2>
       </div>
 
-      <StepsContextProvider>
-        <CreateApplicationStepper>
-          <MainDetails />
-          <TeamInformation />
-          <GrantScoping />
-          <Roadmap />
-          <Resources />
-        </CreateApplicationStepper>
-      </StepsContextProvider>
+      <CreateApplicationForm />
     </>
   );
 }
