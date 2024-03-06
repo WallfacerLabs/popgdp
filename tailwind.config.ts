d933b09fd9f43dada3551cb2c60fedb5fe9117d5
mazya <mazur097mazur@gmail.com>
2024-03-06 17:27:09 +0100

    Update styles confing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,8 +23,6 @@ const config = {
       },
       colors: {
         border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
@@ -35,10 +33,6 @@ const config = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
@@ -47,13 +41,21 @@ const config = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+        green: {
+          DEFAULT: "hsl(var(--green))",
+        },
+        orange: {
+          DEFAULT: "hsl(var(--orange))",
+        },
+        blue: {
+          DEFAULT: "hsl(var(--blue))",
+        },
+        pink: {
+          DEFAULT: "hsl(var(--pink))",
         },
       },
       borderRadius: {
