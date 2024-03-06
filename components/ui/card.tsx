480d2b9f1a980f61b214a4dbbb8b0b3f8079e265
mazya <mazur097mazur@gmail.com>
2024-03-06 17:33:43 +0100

    Remove reduntant ground styling

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -6,10 +6,7 @@ const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn(
-        "rounded-lg border bg-card text-card-foreground shadow-sm",
-        className,
-      )}
+      className={cn("rounded-lg border bg-background shadow-sm", className)}
       {...props}
     />
   ),
