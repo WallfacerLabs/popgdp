098f362c158da19edffe28789cbc2b5b832c8944
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-06 16:08:05 +0100

    🌝 Remove dark mode traces (#59)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
-  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
