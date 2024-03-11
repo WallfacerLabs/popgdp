90099d8927cc891bb05aebb6144531e8f716efa3
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-11 12:37:49 +0100

    🧑‍⚖️ Update tabs section semantic (#78)

--- components/ui/applicationPreview.tsx
+++ components/ui/applicationPreview.tsx
@@ -72,87 +72,89 @@ export function ApplicationPreview({ application }: ApplicationPreviewProps) {
         </div>
         <Image src={projectPlaceholder} alt="" />
       </div>
-      <Tabs
-        defaultValue={TABS_CONFIG[0].key}
-        className="mt-16 flex gap-20"
-        orientation="vertical"
-      >
-        <TabsList className="flex-col items-start justify-start gap-6">
-          {TABS_CONFIG.map(({ key, label, icon }) => (
-            <TabsTrigger
-              value={key}
-              key={key}
-              className="flex items-center gap-1"
-            >
-              <span className="h-4 w-4"> {icon}</span>
-              {label}
-            </TabsTrigger>
-          ))}
-        </TabsList>
-        <TabContainer index={0}>
-          <ContentRow label="Team summary:" vertical>
-            {application.teamSummary}
-          </ContentRow>
-        </TabContainer>
-        <TabContainer index={1}>
-          <ContentRow label="Project idea:" vertical>
-            {application.projectIdea}
-          </ContentRow>
-          <ContentRow label="Why this & why now:" vertical>
-            {application.projectReason}
-          </ContentRow>
-          <ContentRow label="Project goals:" vertical>
-            {application.projectGoals}
-          </ContentRow>
-          <ContentRow label="Project requirements:" vertical>
-            {application.projectRequirements}
-          </ContentRow>
-        </TabContainer>
-        <TabContainer index={2}>
-          <ContentRow label="Lorem ipsum:" vertical>
-            Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh mi
-            netus. Vulputate nunc risus scelerisque risus tortor tempor. Nullam
-            nec sociis aliquet varius enim velit iaculis. Nisi ut lobortis ipsum
-            suspendisse facilisi facilisi neque lectus. Nisl massa id
-            scelerisque ut imperdiet est aliquam arcu erat. Nunc condimentum
-            ultricies quis faucibus ultrices volutpat consequat massa pretium. A
-            augue eros molestie lectus netus. Sit ac eu natoque enim nulla nec
-            sollicitudin adipiscing. Tincidunt porta adipiscing blandit ac
-            suspendisse est venenatis aliquam.
-          </ContentRow>
-          <ContentRow label="Lorem ipsum:" vertical>
-            Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus
-            nisi. Vestibulum eget aliquet risus scelerisque. Amet dolor urna
-            vitae nunc quis gravida ante diam. Ultricies ullamcorper sodales
-            nulla et. Ut viverra vestibulum fames scelerisque amet. Nec non sed
-            non nulla ultrices amet iaculis. Volutpat id urna diam placerat
-            arcu. Posuere eu ut donec nam urna donec. Nunc eget quam justo
-            tellus pulvinar.
-          </ContentRow>
-        </TabContainer>
-        <TabContainer index={3}>
-          <ContentRow label="Lorem ipsum:" vertical>
-            Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh mi
-            netus. Vulputate nunc risus scelerisque risus tortor tempor. Nullam
-            nec sociis aliquet varius enim velit iaculis. Nisi ut lobortis ipsum
-            suspendisse facilisi facilisi neque lectus. Nisl massa id
-            scelerisque ut imperdiet est aliquam arcu erat. Nunc condimentum
-            ultricies quis faucibus ultrices volutpat consequat massa pretium. A
-            augue eros molestie lectus netus. Sit ac eu natoque enim nulla nec
-            sollicitudin adipiscing. Tincidunt porta adipiscing blandit ac
-            suspendisse est venenatis aliquam.
-          </ContentRow>
-          <ContentRow label="Lorem ipsum:" vertical>
-            Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus
-            nisi. Vestibulum eget aliquet risus scelerisque. Amet dolor urna
-            vitae nunc quis gravida ante diam. Ultricies ullamcorper sodales
-            nulla et. Ut viverra vestibulum fames scelerisque amet. Nec non sed
-            non nulla ultrices amet iaculis. Volutpat id urna diam placerat
-            arcu. Posuere eu ut donec nam urna donec. Nunc eget quam justo
-            tellus pulvinar.
-          </ContentRow>
-        </TabContainer>
-      </Tabs>
+      <section className="mt-16">
+        <Tabs
+          defaultValue={TABS_CONFIG[0].key}
+          className="flex gap-20"
+          orientation="vertical"
+        >
+          <TabsList className="flex-col items-start justify-start gap-6">
+            {TABS_CONFIG.map(({ key, label, icon }) => (
+              <TabsTrigger
+                value={key}
+                key={key}
+                className="flex items-center gap-1"
+              >
+                <span className="h-4 w-4"> {icon}</span>
+                {label}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <TabContainer index={0}>
+            <ContentRow label="Team summary:" vertical>
+              {application.teamSummary}
+            </ContentRow>
+          </TabContainer>
+          <TabContainer index={1}>
+            <ContentRow label="Project idea:" vertical>
+              {application.projectIdea}
+            </ContentRow>
+            <ContentRow label="Why this & why now:" vertical>
+              {application.projectReason}
+            </ContentRow>
+            <ContentRow label="Project goals:" vertical>
+              {application.projectGoals}
+            </ContentRow>
+            <ContentRow label="Project requirements:" vertical>
+              {application.projectRequirements}
+            </ContentRow>
+          </TabContainer>
+          <TabContainer index={2}>
+            <ContentRow label="Lorem ipsum:" vertical>
+              Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh
+              mi netus. Vulputate nunc risus scelerisque risus tortor tempor.
+              Nullam nec sociis aliquet varius enim velit iaculis. Nisi ut
+              lobortis ipsum suspendisse facilisi facilisi neque lectus. Nisl
+              massa id scelerisque ut imperdiet est aliquam arcu erat. Nunc
+              condimentum ultricies quis faucibus ultrices volutpat consequat
+              massa pretium. A augue eros molestie lectus netus. Sit ac eu
+              natoque enim nulla nec sollicitudin adipiscing. Tincidunt porta
+              adipiscing blandit ac suspendisse est venenatis aliquam.
+            </ContentRow>
+            <ContentRow label="Lorem ipsum:" vertical>
+              Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus
+              nisi. Vestibulum eget aliquet risus scelerisque. Amet dolor urna
+              vitae nunc quis gravida ante diam. Ultricies ullamcorper sodales
+              nulla et. Ut viverra vestibulum fames scelerisque amet. Nec non
+              sed non nulla ultrices amet iaculis. Volutpat id urna diam
+              placerat arcu. Posuere eu ut donec nam urna donec. Nunc eget quam
+              justo tellus pulvinar.
+            </ContentRow>
+          </TabContainer>
+          <TabContainer index={3}>
+            <ContentRow label="Lorem ipsum:" vertical>
+              Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh
+              mi netus. Vulputate nunc risus scelerisque risus tortor tempor.
+              Nullam nec sociis aliquet varius enim velit iaculis. Nisi ut
+              lobortis ipsum suspendisse facilisi facilisi neque lectus. Nisl
+              massa id scelerisque ut imperdiet est aliquam arcu erat. Nunc
+              condimentum ultricies quis faucibus ultrices volutpat consequat
+              massa pretium. A augue eros molestie lectus netus. Sit ac eu
+              natoque enim nulla nec sollicitudin adipiscing. Tincidunt porta
+              adipiscing blandit ac suspendisse est venenatis aliquam.
+            </ContentRow>
+            <ContentRow label="Lorem ipsum:" vertical>
+              Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus
+              nisi. Vestibulum eget aliquet risus scelerisque. Amet dolor urna
+              vitae nunc quis gravida ante diam. Ultricies ullamcorper sodales
+              nulla et. Ut viverra vestibulum fames scelerisque amet. Nec non
+              sed non nulla ultrices amet iaculis. Volutpat id urna diam
+              placerat arcu. Posuere eu ut donec nam urna donec. Nunc eget quam
+              justo tellus pulvinar.
+            </ContentRow>
+          </TabContainer>
+        </Tabs>
+      </section>
     </div>
   );
 }
@@ -212,12 +214,12 @@ function TabContainer({ children, index }: TabContainerProps) {
   return (
     <TabsContent value={tabConfig.key} className="flex flex-col">
       <div className="flex items-center gap-3 py-2">
-        <span className="h-6 w-6">{tabConfig.icon}</span>
-        {tabConfig.label}
+        <div className="h-6 w-6">{tabConfig.icon}</div>
+        <h4 className="font-bold">{tabConfig.label}</h4>
       </div>
-      <div className="mt-8 flex grow flex-col gap-10 border-l pl-10 text-sm">
+      <article className="mt-8 flex grow flex-col gap-10 border-l pl-10 text-sm">
         {children}
-      </div>
+      </article>
     </TabsContent>
   );
 }
