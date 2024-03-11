8f21c5da2163c569d342c813c8c9764d30e945f8
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-11 17:10:14 +0100

    🏡 Add `getLinkNode` util (#87)

---
+++ lib/getLinkNode.ts
@@ -0,0 +1,16 @@
+import { $isLinkNode, LinkNode } from "@lexical/link";
+import { $isRangeSelection, BaseSelection } from "lexical";
+
+export function $getLinkNode(selection: BaseSelection | null) {
+  let linkNode: LinkNode | null = null;
+  if ($isRangeSelection(selection)) {
+    const anchorNode = selection.anchor.getNode();
+    const anchorParent = anchorNode.getParent();
+    const focusNode = selection.focus.getNode();
+    const focusParent = focusNode.getParent();
+    linkNode = [anchorNode, anchorParent, focusNode, focusParent].find((node) =>
+      $isLinkNode(node),
+    ) as LinkNode | null;
+  }
+  return linkNode;
+}
