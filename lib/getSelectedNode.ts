452e5a91129ba25849dfb0f3844e52dca2c5124a
mazya <mazur097mazur@gmail.com>
2024-03-11 17:06:17 +0100

    Format util

--- lib/getSelectedNode.ts
+++ lib/getSelectedNode.ts
@@ -1,5 +1,5 @@
-import { $isAtNodeEnd } from '@lexical/selection';
-import { ElementNode, RangeSelection, TextNode } from 'lexical';
+import { $isAtNodeEnd } from "@lexical/selection";
+import { ElementNode, RangeSelection, TextNode } from "lexical";
 
 export function getSelectedNode(
   selection: RangeSelection,
