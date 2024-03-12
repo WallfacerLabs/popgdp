0682ddc0a8f5609cf16e7dcb972e4b7b427980d8
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-12 14:01:46 +0100

    😚 Add transition to link underline (#103)

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-primary underline decoration-primary/0 hover:decoration-primary/100 focus-visible:decoration-primary/100 underline-offset-4",
       },
       size: {
         default: "h-10 px-4 py-2",
