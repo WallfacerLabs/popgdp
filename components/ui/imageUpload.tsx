357c61df0b2e642f22732583dd109bd636dbbdf0
mazya <mazur097mazur@gmail.com>
2024-03-12 15:35:14 +0100

    Update image upload component interaction styles

--- components/ui/imageUpload.tsx
+++ components/ui/imageUpload.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import { ChangeEventHandler, HTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
 import { cn } from "@/lib/cn";
@@ -8,8 +8,8 @@ import { UploadCloudIcon } from "@/components/icons/uploadCloudIcon";
 const imageUploadVariants = cva(
   cn(
     "group flex flex-col gap-2 justify-center items-center relative w-full h-full max-w-56 min-h-44 max-h-44 border rounded-3xl transition-colors cursor-pointer text-center",
-    "hover:border-primary focus-within:border-primary [&:has(input:focus):has(input:focus-visible)]:border-primary",
-    "[&:has(input:disabled)]:cursor-not-allowed",
+    "[&:hover:not(:has(input:disabled))]:border-primary [&:has(input:focus):not(:has(input:disabled))]:border-primary",
+    "[&:has(input:disabled)]:cursor-not-allowed [&:has(input:disabled)]:opacity-50",
   ),
   {
     variants: {
@@ -26,21 +26,29 @@ const imageUploadVariants = cva(
 
 interface ImageUploadProps extends HTMLAttributes<HTMLInputElement> {
   placeholder?: string;
+  disabled?: boolean;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
 }
 
 export const ImageUpload = ({
   className,
-  onChange,
   placeholder,
+  disabled,
+  onChange,
 }: ImageUploadProps) => {
   return (
     <label className={imageUploadVariants({ variant: "empty", className })}>
-      <UploadCloudIcon className="h-6 w-6 text-border transition-colors group-hover:text-primary" />
-      {placeholder && <span className="text-sm font-bold">{placeholder}</span>}
+      <UploadCloudIcon className="h-6 w-6 text-border transition-colors group-[&:has(input:focus):not(:has(input:disabled))]:text-primary group-[&:hover:not(:has(input:disabled))]:text-primary" />
+      {placeholder && (
+        <span className="text-sm font-bold group-[&:has(input:disabled)]:opacity-50">
+          {placeholder}
+        </span>
+      )}
       <Input
         type="file"
+        disabled={disabled}
         onChange={onChange}
-        className="-z-1 absolute h-0 w-0 overflow-hidden opacity-0"
+        className="-z-1 disabled:opacity:0 absolute h-0 w-0 overflow-hidden border-0 p-0 opacity-0"
       />
     </label>
   );
