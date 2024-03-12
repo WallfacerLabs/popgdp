c5c79e8f1e4a684ae8c6b95a3d9929af633ddc5c
mazya <mazur097mazur@gmail.com>
2024-03-12 17:13:33 +0100

    Remove image upload style variants

--- components/ui/imageUpload.tsx
+++ components/ui/imageUpload.tsx
@@ -1,28 +1,8 @@
 import { ChangeEventHandler, HTMLAttributes } from "react";
-import { cva } from "class-variance-authority";
 
 import { cn } from "@/lib/cn";
 import { Input } from "@/components/ui/input";
 import { UploadCloudIcon } from "@/components/icons/uploadCloudIcon";
-
-const imageUploadVariants = cva(
-  cn(
-    "group flex flex-col gap-2 justify-center items-center relative w-full h-full max-w-56 min-h-44 max-h-44 border rounded-3xl transition-colors cursor-pointer text-center",
-    "[&:hover:not(:has(input:disabled))]:border-primary [&:has(input:focus):not(:has(input:disabled))]:border-primary",
-    "[&:has(input:disabled)]:cursor-not-allowed [&:has(input:disabled)]:opacity-50",
-  ),
-  {
-    variants: {
-      variant: {
-        empty: "border-dashed",
-        filled: "border-solid",
-      },
-    },
-    defaultVariants: {
-      variant: "empty",
-    },
-  },
-);
 
 interface ImageUploadProps extends HTMLAttributes<HTMLInputElement> {
   placeholder?: string;
@@ -37,7 +17,14 @@ export const ImageUpload = ({
   onChange,
 }: ImageUploadProps) => {
   return (
-    <label className={imageUploadVariants({ variant: "empty", className })}>
+    <label
+      className={cn(
+        "group relative flex h-full max-h-44 min-h-44 w-full max-w-56 cursor-pointer flex-col items-center justify-center gap-2 rounded-3xl border border-dashed text-center transition-colors",
+        "[&:has(input:focus):not(:has(input:disabled))]:border-primary [&:hover:not(:has(input:disabled))]:border-primary",
+        "[&:has(input:disabled)]:cursor-not-allowed [&:has(input:disabled)]:opacity-50",
+        className,
+      )}
+    >
       <UploadCloudIcon className="h-6 w-6 text-border transition-colors group-[&:has(input:focus):not(:has(input:disabled))]:text-primary group-[&:hover:not(:has(input:disabled))]:text-primary" />
       {placeholder && (
         <span className="text-sm font-bold group-[&:has(input:disabled)]:opacity-50">
