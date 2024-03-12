2d9a93dc3be258ac7b94075d050f2254f0d038ea
mazya <mazur097mazur@gmail.com>
2024-03-12 17:11:19 +0100

    Update button variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,19 +5,30 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-2xl text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+  cn(
+    "relative inline-flex items-center justify-center whitespace-nowrap rounded-2xl text-sm font-medium ring-offset-background transition-colors border",
+    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+  ),
   {
     variants: {
       variant: {
-        primary: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline decoration-primary/0 hover:decoration-primary/100 focus-visible:decoration-primary/100 underline-offset-4",
+        primary: cn(
+          "bg-primary border-primary text-primary-foreground",
+          "hover:bg-background hover:text-primary focus-visible:bg-background focus-visible:text-primary",
+        ),
+        secondary: cn(
+          "border-primary bg-background",
+          "hover:bg-primary hover:text-background focus-visible:bg-primary focus-visible:text-background",
+        ),
+        outline: cn(
+          "border bg-background",
+          "hover:bg-border focus-visible:bg-border",
+        ),
+        link: cn(
+          "border-0",
+          "before:absolute before:bottom-0 before:h-0.5 before:w-full before:bg-primary/10 before:transition-colors before:rounded-sm",
+          "hover:before:bg-primary focus-visible:before:bg-primary",
+        ),
       },
       size: {
         default: "h-10 px-4 py-2",
