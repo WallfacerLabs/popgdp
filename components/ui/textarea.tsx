4c9675b3492723b3228471a4ccbfb1a6b53ac05f
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-13 11:48:54 +0100

    📐 Remove resize from textarea (#112)

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,15 +2,14 @@ import { forwardRef, type TextareaHTMLAttributes } from "react";
 
 import { cn } from "@/lib/cn";
 
-export interface TextareaProps
-  extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
         className={cn(
-          "flex min-h-40 w-full rounded-md border bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-40 w-full resize-none rounded-md border bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
