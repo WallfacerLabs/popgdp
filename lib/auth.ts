b9e693c445c852b2b8b1fd728b4c7818f9b7bf40
nezouse <stankiewicz1998@gmail.com>
2024-03-13 12:16:47 +0100

    Remove unused stuff

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,4 @@
-import { db } from "@/drizzle/db";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
-import x from "next-auth/providers/azure-ad-b2c";
 import GithubProvider from "next-auth/providers/github";
 
 export const {
