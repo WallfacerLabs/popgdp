a964cc10b2d80747a4741763574e5afe611140a2
mazya <mazur097mazur@gmail.com>
2024-03-13 11:10:18 +0100

    Change button radiuses

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -32,8 +32,6 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
     },
@@ -46,7 +44,7 @@ const buttonVariants = cva(
 
 interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
