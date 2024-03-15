47ca2db922b1e3932223f7d515d0e2265593bfa9
mazya <mazur097mazur@gmail.com>
2024-03-15 17:50:51 +0100

    Extract section from details

--- src/components/ui/applicationPreview/applicationDetails.tsx
+++ src/components/ui/applicationPreview/applicationDetails.tsx
@@ -39,85 +39,79 @@ export const ApplicationDetails = ({
   application,
 }: ApplicationDetailsProps) => {
   return (
-    <section className="mt-16 border border-[red]">
-      <Tabs
-        defaultValue={TABS_CONFIG[0].key}
-        className="flex gap-20"
-        orientation="vertical"
-      >
-        <TabsList className="flex-col items-start justify-start gap-6">
-          {TABS_CONFIG.map(({ key, label, icon }) => (
-            <TabsTrigger
-              value={key}
-              key={key}
-              className="flex items-center gap-1"
-            >
-              <span className="h-4 w-4"> {icon}</span>
-              {label}
-            </TabsTrigger>
-          ))}
-        </TabsList>
-        <TabContainer index={0}>
-          <ContentRow label="Team summary:">
-            {application.teamSummary}
-          </ContentRow>
-        </TabContainer>
-        <TabContainer index={1}>
-          <ContentRow label="Project idea:">{application.idea}</ContentRow>
-          <ContentRow label="Why this & why now:">
-            {application.reason}
-          </ContentRow>
-          <ContentRow label="Project goals:">{application.goals}</ContentRow>
-          <ContentRow label="Project requirements:">
-            {application.requirements}
-          </ContentRow>
-        </TabContainer>
-        <TabContainer index={2}>
-          <ContentRow label="Lorem ipsum:">
-            Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh mi
-            netus. Vulputate nunc risus scelerisque risus tortor tempor. Nullam
-            nec sociis aliquet varius enim velit iaculis. Nisi ut lobortis ipsum
-            suspendisse facilisi facilisi neque lectus. Nisl massa id
-            scelerisque ut imperdiet est aliquam arcu erat. Nunc condimentum
-            ultricies quis faucibus ultrices volutpat consequat massa pretium. A
-            augue eros molestie lectus netus. Sit ac eu natoque enim nulla nec
-            sollicitudin adipiscing. Tincidunt porta adipiscing blandit ac
-            suspendisse est venenatis aliquam.
-          </ContentRow>
-          <ContentRow label="Lorem ipsum:">
-            Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus
-            nisi. Vestibulum eget aliquet risus scelerisque. Amet dolor urna
-            vitae nunc quis gravida ante diam. Ultricies ullamcorper sodales
-            nulla et. Ut viverra vestibulum fames scelerisque amet. Nec non sed
-            non nulla ultrices amet iaculis. Volutpat id urna diam placerat
-            arcu. Posuere eu ut donec nam urna donec. Nunc eget quam justo
-            tellus pulvinar.
-          </ContentRow>
-        </TabContainer>
-        <TabContainer index={3}>
-          <ContentRow label="Lorem ipsum:">
-            Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh mi
-            netus. Vulputate nunc risus scelerisque risus tortor tempor. Nullam
-            nec sociis aliquet varius enim velit iaculis. Nisi ut lobortis ipsum
-            suspendisse facilisi facilisi neque lectus. Nisl massa id
-            scelerisque ut imperdiet est aliquam arcu erat. Nunc condimentum
-            ultricies quis faucibus ultrices volutpat consequat massa pretium. A
-            augue eros molestie lectus netus. Sit ac eu natoque enim nulla nec
-            sollicitudin adipiscing. Tincidunt porta adipiscing blandit ac
-            suspendisse est venenatis aliquam.
-          </ContentRow>
-          <ContentRow label="Lorem ipsum:">
-            Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus
-            nisi. Vestibulum eget aliquet risus scelerisque. Amet dolor urna
-            vitae nunc quis gravida ante diam. Ultricies ullamcorper sodales
-            nulla et. Ut viverra vestibulum fames scelerisque amet. Nec non sed
-            non nulla ultrices amet iaculis. Volutpat id urna diam placerat
-            arcu. Posuere eu ut donec nam urna donec. Nunc eget quam justo
-            tellus pulvinar.
-          </ContentRow>
-        </TabContainer>
-      </Tabs>
-    </section>
+    <Tabs
+      defaultValue={TABS_CONFIG[0].key}
+      className="flex gap-20"
+      orientation="vertical"
+    >
+      <TabsList className="flex-col items-start justify-start gap-6">
+        {TABS_CONFIG.map(({ key, label, icon }) => (
+          <TabsTrigger
+            value={key}
+            key={key}
+            className="flex items-center gap-1"
+          >
+            <span className="h-4 w-4"> {icon}</span>
+            {label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      <TabContainer index={0}>
+        <ContentRow label="Team summary:">{application.teamSummary}</ContentRow>
+      </TabContainer>
+      <TabContainer index={1}>
+        <ContentRow label="Project idea:">{application.idea}</ContentRow>
+        <ContentRow label="Why this & why now:">
+          {application.reason}
+        </ContentRow>
+        <ContentRow label="Project goals:">{application.goals}</ContentRow>
+        <ContentRow label="Project requirements:">
+          {application.requirements}
+        </ContentRow>
+      </TabContainer>
+      <TabContainer index={2}>
+        <ContentRow label="Lorem ipsum:">
+          Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh mi
+          netus. Vulputate nunc risus scelerisque risus tortor tempor. Nullam
+          nec sociis aliquet varius enim velit iaculis. Nisi ut lobortis ipsum
+          suspendisse facilisi facilisi neque lectus. Nisl massa id scelerisque
+          ut imperdiet est aliquam arcu erat. Nunc condimentum ultricies quis
+          faucibus ultrices volutpat consequat massa pretium. A augue eros
+          molestie lectus netus. Sit ac eu natoque enim nulla nec sollicitudin
+          adipiscing. Tincidunt porta adipiscing blandit ac suspendisse est
+          venenatis aliquam.
+        </ContentRow>
+        <ContentRow label="Lorem ipsum:">
+          Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus nisi.
+          Vestibulum eget aliquet risus scelerisque. Amet dolor urna vitae nunc
+          quis gravida ante diam. Ultricies ullamcorper sodales nulla et. Ut
+          viverra vestibulum fames scelerisque amet. Nec non sed non nulla
+          ultrices amet iaculis. Volutpat id urna diam placerat arcu. Posuere eu
+          ut donec nam urna donec. Nunc eget quam justo tellus pulvinar.
+        </ContentRow>
+      </TabContainer>
+      <TabContainer index={3}>
+        <ContentRow label="Lorem ipsum:">
+          Lorem ipsum dolor sit amet consectetur. Id odio sapien mi ut nibh mi
+          netus. Vulputate nunc risus scelerisque risus tortor tempor. Nullam
+          nec sociis aliquet varius enim velit iaculis. Nisi ut lobortis ipsum
+          suspendisse facilisi facilisi neque lectus. Nisl massa id scelerisque
+          ut imperdiet est aliquam arcu erat. Nunc condimentum ultricies quis
+          faucibus ultrices volutpat consequat massa pretium. A augue eros
+          molestie lectus netus. Sit ac eu natoque enim nulla nec sollicitudin
+          adipiscing. Tincidunt porta adipiscing blandit ac suspendisse est
+          venenatis aliquam.
+        </ContentRow>
+        <ContentRow label="Lorem ipsum:">
+          Lorem ipsum dolor sit amet consectetur. Et faucibus ac faucibus nisi.
+          Vestibulum eget aliquet risus scelerisque. Amet dolor urna vitae nunc
+          quis gravida ante diam. Ultricies ullamcorper sodales nulla et. Ut
+          viverra vestibulum fames scelerisque amet. Nec non sed non nulla
+          ultrices amet iaculis. Volutpat id urna diam placerat arcu. Posuere eu
+          ut donec nam urna donec. Nunc eget quam justo tellus pulvinar.
+        </ContentRow>
+      </TabContainer>
+    </Tabs>
   );
 };
 
