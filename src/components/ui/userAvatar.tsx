7116003947c685f92ed40a4457a5c4739d06731d
mazya <mazur097mazur@gmail.com>
2024-03-18 11:07:39 +0100

    Pass fallback prop to user avatar

--- src/components/ui/userAvatar.tsx
+++ src/components/ui/userAvatar.tsx
@@ -1,15 +1,17 @@
+import { ReactNode } from "react";
+
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 
 interface UserAvatarProps {
   name: string | undefined | null;
   image: string | undefined | null;
+  fallback?: ReactNode;
 }
 
-export function UserAvatar({ name, image }: UserAvatarProps) {
+export function UserAvatar({ name, image, fallback }: UserAvatarProps) {
   return (
     <Avatar>
-      <AvatarFallback />
-
+      <AvatarFallback>{fallback}</AvatarFallback>
       <AvatarImage
         src={image || undefined}
         alt={name ? `${name} avatar` : undefined}
