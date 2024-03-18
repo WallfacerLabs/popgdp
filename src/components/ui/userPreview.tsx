5db77030f96874a6afcf3f12854b33cbfcc08951
mazya <mazur097mazur@gmail.com>
2024-03-18 11:08:48 +0100

    Add user preview

--- src/components/ui/userPreview.tsx
+++ src/components/ui/userPreview.tsx
@@ -1,6 +1,6 @@
 import { UserAvatar } from "./userAvatar";
 
-interface UserPreviewProps {
+export interface UserPreviewProps {
   image: string | undefined | null;
   name: string | undefined | null;
   role: string | undefined | null;
@@ -9,10 +9,13 @@ interface UserPreviewProps {
 export function UserPreview({ image, name, role }: UserPreviewProps) {
   return (
     <div className="flex items-center gap-2">
-      <UserAvatar name={name} image={image} />
+      <UserAvatar
+        name={name}
+        image={image}
+      />
       <div className="flex flex-col gap-1">
         <span className="text-sm font-bold">{name}</span>
-        <span className="text-sm">{role}</span>
+        <span className="text-sm opacity-60">{role}</span>
       </div>
     </div>
   );
