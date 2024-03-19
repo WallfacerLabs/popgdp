bff35fa1bc8d832e2dc064e48366081b5af0b820
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-03-19 14:47:20 +0100

    🍓 Extract stepper component (#171)

--- src/app/waves/[waveId]/applications/create/createApplicationForm.tsx
+++ src/app/waves/[waveId]/applications/create/createApplicationForm.tsx
@@ -1,20 +1,49 @@
-import { CreateApplicationStepper } from "./stepper";
+"use client";
+
+import { Stepper } from "@/components/ui/stepper";
+import { CampaignIcon } from "@/components/icons/campaignIcon";
+import { ClockIcon } from "@/components/icons/clockIcon";
+import { CloudIcon } from "@/components/icons/cloudIcon";
+import { ComputerIcon } from "@/components/icons/computerIcon";
+import { TeamIcon } from "@/components/icons/teamIcon";
+
 import { GrantScoping } from "./steps/grantScoping";
 import { MainDetails } from "./steps/mainDetails";
 import { Resources } from "./steps/resources";
 import { Roadmap } from "./steps/roadmap";
 import { TeamInformation } from "./steps/teamInformation/teamInformation";
+import { useStepsContext } from "./stepsProvider";
+
+const stepsConfig = [
+  { name: "Main details", icon: <ComputerIcon className="h-6 w-6" /> },
+  {
+    name: "Team information",
+    icon: <TeamIcon className="h-6 w-6" />,
+  },
+  {
+    name: "Grant scoping",
+    icon: <CampaignIcon className="h-6 w-6" />,
+  },
+  {
+    name: "Roadmap & timeline",
+    icon: <ClockIcon className="h-6 w-6" />,
+  },
+  {
+    name: "Resources & sustainability",
+    icon: <CloudIcon className="h-6 w-6" />,
+  },
+];
 
 export function Form() {
+  const { currentStep } = useStepsContext();
+
   return (
-    <>
-      <CreateApplicationStepper>
-        <MainDetails />
-        <TeamInformation />
-        <GrantScoping />
-        <Roadmap />
-        <Resources />
-      </CreateApplicationStepper>
-    </>
+    <Stepper currentStep={currentStep} stepsConfig={stepsConfig}>
+      <MainDetails />
+      <TeamInformation />
+      <GrantScoping />
+      <Roadmap />
+      <Resources />
+    </Stepper>
   );
 }
---
+++ src/app/waves/create/steps/mainDetails.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  addDays,
+  formatDate,
+  formatDateRange,
+  getStartOfDate,
+} from "@/lib/dates";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormFooter,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CalendarIcon } from "@/components/icons/calendarIcon";
+
+import { createWaveAction } from "../createWaveAction";
+import { createWaveSchema } from "../createWaveSchema";
+
+export function MainDetails() {
+  const todayDate = getStartOfDate(new Date());
+
+  const form = useForm<createWaveSchema>({
+    resolver: zodResolver(createWaveSchema),
+    defaultValues: {
+      waveName: "",
+      duration: {
+        from: todayDate,
+        to: addDays(todayDate, 30),
+      },
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex w-full flex-col gap-6"
+        onSubmit={form.handleSubmit(async (data) => {
+          await createWaveAction(data);
+        })}
+      >
+        <FormField
+          control={form.control}
+          name="waveName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Wave name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="duration"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Wave duration</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      className="pl-3 text-left font-normal"
+                    >
+                      {field.value.from ? (
+                        field.value.to ? (
+                          <>
+                            {formatDateRange(field.value.from, field.value.to)}
+                          </>
+                        ) : (
+                          formatDate(field.value.from)
+                        )
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="range"
+                    numberOfMonths={2}
+                    defaultMonth={field.value.from}
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) => date < todayDate}
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormFooter className="justify-end">
+          <Button disabled={form.formState.isSubmitting}>Create wave</Button>
+        </FormFooter>
+      </form>
+    </Form>
+  );
+}
