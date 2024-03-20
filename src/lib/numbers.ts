7537bfe0d6068f4122dee16d93ae8e9bd217aea9
mazya <mazur097mazur@gmail.com>
2024-03-20 12:29:22 +0100

    Add number formatter

---
+++ src/lib/numbers.ts
@@ -0,0 +1,22 @@
+type FractionsConfig = {
+  maximumFractionDigits?: number;
+  minimumFractionDigits?: number;
+};
+
+const fractionsConfigDefault: FractionsConfig = {
+  maximumFractionDigits: 2,
+};
+
+export function formatNumber(
+  value: number,
+  fractionsConfig: FractionsConfig = fractionsConfigDefault,
+) {
+  const formatterConfig = {
+    ...fractionsConfig,
+    compactDisplay: "short",
+    notation: "compact",
+    roundingMode: "floor",
+  } as Intl.NumberFormatOptions;
+  const formatter = new Intl.NumberFormat("en-US", formatterConfig);
+  return formatter.format(value);
+}
