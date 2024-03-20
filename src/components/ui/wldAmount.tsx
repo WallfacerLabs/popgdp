1d0b8108be408c326dce7302263f4708e5519ea3
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-20 16:19:10 +0100

    🈷️ Show application budget amount in table (#184)

--- src/components/ui/wldAmount.tsx
+++ src/components/ui/wldAmount.tsx
@@ -1,9 +1,10 @@
+import { formatNumber } from "@/lib/numbers";
 import { WorldcoinIcon } from "@/components/icons/worldcoinIcon";
 
 export function WldAmount({ amount }: { amount: number }) {
   return (
     <div className="flex items-center">
-      <span className="font-bold">{amount}</span>
+      <span className="font-bold">{formatNumber(amount)}</span>
       <WorldcoinIcon className="ml-2 mr-1 h-4 w-4" />
       <span className="text-foreground/60">WLD</span>
     </div>
