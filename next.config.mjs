67abf41c622fc54d840ff88f7ff94166681d0764
nezouse <stankiewicz1998@gmail.com>
2024-03-20 17:40:54 +0100

    ⚡️ Add source maps on prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 import { withAxiom } from "next-axiom";
 
-const nextConfig = withAxiom({});
+const nextConfig = withAxiom({
+  productionBrowserSourceMaps: true,
+});
 
 export default nextConfig;
