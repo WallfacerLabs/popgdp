61f814b87b2a92a61c0f8628f45e1ac9792555fb
mazya <mazur097mazur@gmail.com>
2024-03-20 11:48:22 +0100

    Update number schema

--- src/constants/validationSchemas.ts
+++ src/constants/validationSchemas.ts
@@ -6,13 +6,14 @@ export const numberSchema = (fieldName: string) => {
   return z
     .string()
     .min(1, { message: errorMessages.nonEmptyString(fieldName) })
-    .regex(/^(0?\.\d+|\d+(\.\d+)?)$/, {
+    .regex(/^[-]?(0?\.\d+|\d+(\.\d+)?)$/, {
       message: errorMessages.number(fieldName),
-    });
+    })
+    .transform((value) => Number(value));
 };
 
 export const positiveNumberSchema = (fieldName: string) => {
-  return numberSchema(fieldName).refine((value) => Number(value) > 0, {
+  return numberSchema(fieldName).refine((value) => value > 0, {
     message: errorMessages.positiveNumber(fieldName),
   });
 };
