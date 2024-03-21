a836e2354cc40439ea7c4be2f64af50ac4885a59
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-03-21 15:52:27 +0100

    📻 Add radio group (#204)

---
+++ src/components/ui/radioGroup.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+
+import { cn } from "@/lib/cn";
+
+const RadioGroup = forwardRef<
+  ElementRef<typeof RadioGroupPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn("grid gap-2", className)}
+      {...props}
+      ref={ref}
+    />
+  );
+});
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+
+const RadioGroupItem = forwardRef<
+  ElementRef<typeof RadioGroupPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "aspect-square h-4 w-4 rounded-full border border-transparent text-background transition-colors",
+        "data-[state=checked]:border-primary data-[state=checked]:text-primary",
+        "focus:outline-none focus-visible:border-primary",
+        "disabled:cursor-not-allowed disabled:opacity-50",
+        className,
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+        <div className="aspect-square h-3 w-3 rounded-full bg-primary transition-colors" />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  );
+});
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
+
+const RadioGroupLabel = forwardRef<
+  ElementRef<typeof LabelPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(
+      "flex cursor-pointer items-center justify-between gap-2 rounded-2xl border p-3 text-xs font-medium leading-none transition-colors",
+      "focus-within:border-primary [&:has([data-state=checked])]:border-primary [&:hover:not(:has(:disabled))]:border-primary",
+      "has-[:disabled]:cursor-not-allowed has-[:disabled]:opacity-50",
+      className,
+    )}
+    {...props}
+  />
+));
+RadioGroupLabel.displayName = LabelPrimitive.Root.displayName;
+
+export { RadioGroup, RadioGroupItem, RadioGroupLabel };
