8d54837aa560e4d4c83f0ea070d05fa14f787e68
mazya <mazur097mazur@gmail.com>
2024-03-26 11:44:25 +0100

    Run prettier

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -27,7 +27,7 @@ const badgeVariants = cva(
 
 export interface BadgeProps
   extends HTMLAttributes<HTMLDivElement>,
-  VariantProps<typeof badgeVariants> { }
+    VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
