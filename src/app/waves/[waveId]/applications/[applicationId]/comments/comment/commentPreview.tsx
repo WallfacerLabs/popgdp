32f92b7f71c40ca27a81c941e3b369043479b0dd
mazya <mazur097mazur@gmail.com>
2024-03-27 12:59:34 +0100

    Pass reply form to comment preview

--- src/app/waves/[waveId]/applications/[applicationId]/comments/comment/commentPreview.tsx
+++ src/app/waves/[waveId]/applications/[applicationId]/comments/comment/commentPreview.tsx
@@ -15,50 +15,55 @@ import { CommentProps } from "./comment";
 interface CommentPreviewProps extends Omit<CommentProps, "waveId" | "userId"> {
   commentContent: ReactNode;
   commentValueForm: ReactNode;
+  commentReplyForm: ReactNode;
 }
 
 export const CommentPreview = ({
   comment,
   commentContent,
   commentValueForm,
+  commentReplyForm,
 }: CommentPreviewProps) => {
   const [isReply, setIsReply] = useState(false);
   const isReview = comment.review?.isReview;
 
   return (
-    <article className={commentContainerVariants({ isReview })}>
-      {isReview && (
-        <Badge variant="orange" className="flex w-fit">
-          Review
-        </Badge>
-      )}
-      <div className="flex gap-3">
-        <UserAvatar name={comment.user.name} imageId={comment.user.imageId} />
-        <div className="flex w-full flex-col gap-1">
-          <div className="flex flex-wrap items-center justify-between gap-x-4 gap-y-1">
-            <div className="flex items-center gap-2 text-sm">
-              <span className="font-bold">{comment.user.name}</span>
-              <Separator className="h-0.5 w-0.5 rounded-full bg-primary opacity-60" />
-              <span className="opacity-60">Member</span>
-              <Separator className="h-0.5 w-0.5 rounded-full bg-primary opacity-60" />
-              <span className="opacity-60">
-                {formatTime(comment.createdAt)}
-              </span>
+    <div className="flex flex-col gap-4">
+      <article className={commentContainerVariants({ isReview })}>
+        {isReview && (
+          <Badge variant="orange" className="flex w-fit">
+            Review
+          </Badge>
+        )}
+        <div className="flex gap-3">
+          <UserAvatar name={comment.user.name} imageId={comment.user.imageId} />
+          <div className="flex w-full flex-col gap-1">
+            <div className="flex flex-wrap items-center justify-between gap-x-4 gap-y-1">
+              <div className="flex items-center gap-2 text-sm">
+                <span className="font-bold">{comment.user.name}</span>
+                <Separator className="h-0.5 w-0.5 rounded-full bg-primary opacity-60" />
+                <span className="opacity-60">Member</span>
+                <Separator className="h-0.5 w-0.5 rounded-full bg-primary opacity-60" />
+                <span className="opacity-60">
+                  {formatTime(comment.createdAt)}
+                </span>
+              </div>
+              <Button
+                variant="link"
+                className="h-6 p-2 py-0 opacity-60 transition-opacity before:opacity-0 hover:opacity-100 focus-visible:opacity-100"
+                onClick={() => setIsReply(true)}
+              >
+                <ReplyIcon />
+                Reply
+              </Button>
             </div>
-            <Button
-              variant="link"
-              className="h-6 p-2 py-0 opacity-60 transition-opacity before:opacity-0 hover:opacity-100 focus-visible:opacity-100"
-              onClick={() => setIsReply(true)}
-            >
-              <ReplyIcon />
-              Reply {isReply && "ing"}
-            </Button>
+            {commentContent}
+            {commentValueForm}
           </div>
-          {commentContent}
-          {commentValueForm}
         </div>
-      </div>
-    </article>
+      </article>
+      {isReply && commentReplyForm}
+    </div>
   );
 };
 
