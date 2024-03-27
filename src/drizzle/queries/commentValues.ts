99147cfca365cea20f3298a93bd602cedf9fc680
nezouse <stankiewicz1998@gmail.com>
2024-03-27 13:40:01 +0100

    Remove query

--- src/drizzle/queries/commentValues.ts
+++ src/drizzle/queries/commentValues.ts
@@ -1,27 +1,6 @@
-import { cache } from "react";
 import { db } from "@/drizzle/db";
 import { CommentValue } from "@/drizzle/schema";
 import { and, eq } from "drizzle-orm";
-
-type CommentValueQuery = {
-  commentId: (typeof CommentValue.$inferSelect)["commentId"];
-  userId: (typeof CommentValue.$inferSelect)["userId"] | undefined;
-};
-
-export const getCommentValue = cache(async (query: CommentValueQuery) => {
-  if (!query.userId) {
-    return undefined;
-  }
-
-  const result = await db.query.CommentValue.findFirst({
-    where: and(
-      eq(CommentValue.commentId, query.commentId),
-      eq(CommentValue.userId, query.userId),
-    ),
-  });
-
-  return result?.value;
-});
 
 export function insertCommentValue(data: typeof CommentValue.$inferInsert) {
   return db
