f1fbbc952abe3f1d646f8b2888fcd5f6e23db58c
nezouse <stankiewicz1998@gmail.com>
2024-03-28 14:29:26 +0100

    🏃‍♂️ Update add category button

--- src/app/waves/create/steps/mainDetails.tsx
+++ src/app/waves/create/steps/mainDetails.tsx
@@ -128,7 +128,7 @@ export function MainDetails() {
             }
           >
             <PlusCircleIcon />
-            Add member
+            Add category
           </Button>
         </fieldset>
 
