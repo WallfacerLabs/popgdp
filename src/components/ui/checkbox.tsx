ff4efab6a137cacbd56837e205cb254d45ff209c
mazya <mazur097mazur@gmail.com>
2024-03-28 16:40:33 +0100

    Add transition to checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -14,10 +14,10 @@ export const Checkbox = forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-6 w-6 shrink-0 rounded-sm border border-primary ring-offset-background",
+      "peer h-6 w-6 shrink-0 rounded-sm border border-primary text-background ring-offset-background transition-colors",
       "focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
       "disabled:cursor-not-allowed disabled:opacity-50",
-      "data-[state=checked]:bg-primary data-[state=checked]:text-background",
+      "data-[state=checked]:bg-primary",
       className,
     )}
     {...props}
