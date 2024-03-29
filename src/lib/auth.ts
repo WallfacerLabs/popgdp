9646df6017ab384d20732155ea3499bc03568bb1
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-03-29 15:44:33 +0100

    👒 Add stage action checks function (#287)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,3 +17,93 @@ export async function getUserId(): Promise<UserId | undefined> {
 
   return user.data.sid;
 }
+
+type WaveStage = "notOpen" | "open" | "denoising" | "assesment" | "closed";
+
+interface WaveStageArgs {
+  openStartDate: Date;
+  denoisingStartDate: Date;
+  assesmentStartDate: Date;
+  closeDate: Date;
+}
+
+export function getWaveStage({
+  openStartDate,
+  denoisingStartDate,
+  assesmentStartDate,
+  closeDate,
+}: WaveStageArgs): WaveStage {
+  const currentDate = new Date();
+
+  if (currentDate > closeDate) {
+    return "closed";
+  }
+
+  if (currentDate > assesmentStartDate) {
+    return "assesment";
+  }
+
+  if (currentDate > denoisingStartDate) {
+    return "denoising";
+  }
+
+  if (currentDate > openStartDate) {
+    return "open";
+  }
+
+  return "notOpen";
+}
+
+type UserAction =
+  | "submissionAdd"
+  | "submissionEdit"
+  | "submissionSpam"
+  | "submissionVote"
+  | "commentAdd"
+  | "commentValue"
+  | "reviewAdd";
+
+// prettier-ignore
+const canPerformByStage = {
+  open: {
+    submissionAdd:  true,
+    submissionEdit: true,
+    submissionSpam: true,
+    submissionVote: false,
+    commentAdd:     true,
+    commentValue:   true,
+    reviewAdd:      false,
+  },
+  denoising: {
+    submissionAdd:  false,
+    submissionEdit: true,
+    submissionSpam: true,
+    submissionVote: false,
+    commentAdd:     true,
+    commentValue:   true,
+    reviewAdd:      false,
+  },
+  assesment: {
+    submissionAdd:  false,
+    submissionEdit: false,
+    submissionSpam: true,
+    submissionVote: true,
+    commentAdd:     true,
+    commentValue:   true,
+    reviewAdd:      false,
+  },
+} satisfies Record<
+  Exclude<WaveStage, "notOpen" | "closed">,
+  Record<UserAction, boolean>
+>;
+
+export function canPerformActionByStage(
+  stage: WaveStage,
+  action: UserAction,
+): boolean {
+  if (stage === "notOpen" || stage === "closed") {
+    return false;
+  }
+
+  return canPerformByStage[stage][action];
+}
