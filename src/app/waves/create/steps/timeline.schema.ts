e6a2820022a9ca75c3dcaf8ec5aa03a654c8cb4a
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-04-02 13:04:19 +0200

    🕧 Make timeline schema sequent (#293)

--- src/app/waves/create/steps/timeline.schema.ts
+++ src/app/waves/create/steps/timeline.schema.ts
@@ -1,3 +1,4 @@
+import { errorMessages } from "@/constants/errorMessages";
 import { z } from "zod";
 
 export const timelineSchema = z.object({
@@ -6,5 +7,22 @@ export const timelineSchema = z.object({
   assesmentStartDate: z.coerce.date(),
   closeDate: z.coerce.date(),
 });
+
+export const sequentTimelineSchema = timelineSchema
+  .refine((schema) => schema.denoisingStartDate > schema.openStartDate, {
+    message: errorMessages.laterDate("Denoising start date", "Open start date"),
+    path: ["denoisingStartDate"],
+  })
+  .refine((schema) => schema.assesmentStartDate > schema.denoisingStartDate, {
+    message: errorMessages.laterDate(
+      "Assesment start date",
+      "Denoising start date",
+    ),
+    path: ["assesmentStartDate"],
+  })
+  .refine((schema) => schema.closeDate > schema.assesmentStartDate, {
+    message: errorMessages.laterDate("Close date", "Assesment start date"),
+    path: ["closeDate"],
+  });
 
 export type timelineSchema = z.infer<typeof timelineSchema>;
