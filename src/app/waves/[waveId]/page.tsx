395d9274efb34c64023894a137ed96b9f3937609
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-04-02 14:48:22 +0200

    🪧 Extract wave preview banner (#296)

--- src/app/waves/[waveId]/page.tsx
+++ src/app/waves/[waveId]/page.tsx
@@ -1,17 +1,16 @@
-import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { urls } from "@/constants/urls";
 import { getWaveWithApplications } from "@/drizzle/queries/waves";
-import globeImage from "@/images/globe.png";
 import { z } from "zod";
 
 import { parseWaveParams } from "@/lib/paramsValidation";
 import { ApplicationsTable } from "@/components/ui/applicationsTable/applicationsTable";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { PageTitle } from "@/components/ui/pageTitle";
 import { TablePagination } from "@/components/ui/pagination/tablePagination";
+
+import { WaveBanner } from "./waveBanner";
 
 const PAGE_SIZE = 10;
 
@@ -45,20 +44,7 @@ export default async function Wave({
 
   return (
     <>
-      <Card className="relative flex-row items-center gap-x-10 py-6 pl-10 pr-64">
-        <CardTitle>POPGDP</CardTitle>
-        <CardContent className="text-xs text-muted-foreground">
-          POPGDP is <b>Proof of Personhood based Grant Distribution Platform</b>
-          . In its first version it is designed to help Worldcoin Foundation
-          review and distribute WLD grants to applicants by sourcing additional
-          “signal” from World ID holders.
-        </CardContent>
-        <Image
-          src={globeImage}
-          alt=""
-          className="absolute bottom-0 right-[32px] h-[125px] w-auto"
-        />
-      </Card>
+      <WaveBanner />
 
       <div className="mt-8 flex items-center justify-between">
         <PageTitle>Submissions</PageTitle>
