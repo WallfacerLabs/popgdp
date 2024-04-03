1475c3fdbe6a4dbfaab00dd62898d48224a8faca
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-04-03 10:51:45 +0200

    👛 Refactor timeline stage (#301)

--- src/app/waves/[waveId]/timeline/timelinePreview.tsx
+++ src/app/waves/[waveId]/timeline/timelinePreview.tsx
@@ -1,5 +1,7 @@
 import { Wave } from "@/types/Wave";
+import { getWaveStage } from "@/lib/auth";
 import { cn } from "@/lib/cn";
+import { Badge } from "@/components/ui/badge";
 
 import { TimelineStage } from "./timelineStage";
 
@@ -9,32 +11,41 @@ interface TimelinePreviewProps {
 }
 
 export const TimelinePreview = ({ wave, className }: TimelinePreviewProps) => {
-  const {
-    name,
-    openStartDate,
-    denoisingStartDate,
-    assesmentStartDate,
-    closeDate,
-  } = wave;
+  const { openStartDate, denoisingStartDate, assesmentStartDate, closeDate } =
+    wave;
+
+  const waveStage = getWaveStage(wave);
 
   return (
-    <ul className={cn("flex rounded-3xl", className)}>
-      <TimelineStage
-        name="Open"
-        startDate={openStartDate}
-        endDate={denoisingStartDate}
-      />
-      <TimelineStage
-        name="Denoising"
-        startDate={denoisingStartDate}
-        endDate={assesmentStartDate}
-      />
-      <TimelineStage
-        name="Assesment"
-        startDate={assesmentStartDate}
-        endDate={closeDate}
-      />
-      <TimelineStage name="Close" startDate={closeDate} />
-    </ul>
+    <div className={cn("flex flex-col items-center gap-2", className)}>
+      {waveStage === "notOpen" && (
+        <Badge className="px-4 font-mono">Wave will start soon</Badge>
+      )}
+      <ul className="flex w-full rounded-3xl  ">
+        <TimelineStage
+          name="open"
+          currentStage={waveStage}
+          startDate={openStartDate}
+          endDate={denoisingStartDate}
+        />
+        <TimelineStage
+          name="denoising"
+          currentStage={waveStage}
+          startDate={denoisingStartDate}
+          endDate={assesmentStartDate}
+        />
+        <TimelineStage
+          name="assesment"
+          currentStage={waveStage}
+          startDate={assesmentStartDate}
+          endDate={closeDate}
+        />
+        <TimelineStage
+          name="close"
+          currentStage={waveStage}
+          startDate={closeDate}
+        />
+      </ul>
+    </div>
   );
 };
