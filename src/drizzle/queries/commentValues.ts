6f85160fb2ca5434615be5b1e1af2c4b1dede8ba
nezouse <stankiewicz1998@gmail.com>
2024-04-08 15:49:01 +0200

    👲 Add subquery

--- src/drizzle/queries/commentValues.ts
+++ src/drizzle/queries/commentValues.ts
@@ -1,6 +1,6 @@
 import { db } from "@/drizzle/db";
 import { CommentValue } from "@/drizzle/schema";
-import { and, eq } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 
 export function insertCommentValue(data: typeof CommentValue.$inferInsert) {
   return db
@@ -22,3 +22,19 @@ export function deleteCommentValue(query: typeof CommentValue.$inferSelect) {
       ),
     );
 }
+
+export const commentValueSq = db
+  .select({
+    userId: CommentValue.userId,
+    spamCount:
+      sql<number>`count(case when ${CommentValue.value} = 'spam' then 1 end)`
+        .mapWith(Number)
+        .as("spamCount"),
+    helpfulCount:
+      sql<number>`count(case when ${CommentValue.value} = 'positive' then 1 end)`
+        .mapWith(Number)
+        .as("helpfulCount"),
+  })
+  .from(CommentValue)
+  .groupBy(CommentValue.userId)
+  .as("commentValueSq");
