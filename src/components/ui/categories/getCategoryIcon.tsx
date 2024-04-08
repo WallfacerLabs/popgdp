b2bfe99284390f23542df62575553a2b72c36a6e
nezouse <stankiewicz1998@gmail.com>
2024-04-08 14:04:57 +0200

    👨‍🍳 Add more icons to picker

--- src/components/ui/categories/getCategoryIcon.tsx
+++ src/components/ui/categories/getCategoryIcon.tsx
@@ -1,13 +1,21 @@
 import { type ReactNode } from "react";
 
 import { type CategoryColor } from "@/types/CategoryColor";
+import { CampaignIcon } from "@/components/icons/campaignIcon";
+import { CloudIcon } from "@/components/icons/cloudIcon";
+import { PictureIcon } from "@/components/icons/pictureIcon";
 import { PlusIcon } from "@/components/icons/plusIcon";
+import { SettingsIcon } from "@/components/icons/settingsIcon";
 import { SunIcon } from "@/components/icons/sunIcon";
 import { ThumbUpIcon } from "@/components/icons/thumbUpIcon";
 
 const categoryIcons = {
   red: <SunIcon />,
   pink: <ThumbUpIcon />,
+  blue: <CloudIcon />,
+  orange: <SettingsIcon />,
+  purple: <CampaignIcon />,
+  green: <PictureIcon />,
 } satisfies Record<CategoryColor, ReactNode>;
 
 export function getCategoryIcon(
---
+++ src/drizzle/migrations/0015_next_silhouette.sql
@@ -0,0 +1,4 @@
+ALTER TYPE "categoryColor" ADD VALUE IF NOT EXISTS 'orange';
+ALTER TYPE "categoryColor" ADD VALUE IF NOT EXISTS 'green';
+ALTER TYPE "categoryColor" ADD VALUE IF NOT EXISTS 'blue';
+ALTER TYPE "categoryColor" ADD VALUE IF NOT EXISTS 'purple';
