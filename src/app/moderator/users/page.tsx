9ff25ff4607b62d4076e6d32aab6640557a5adf0
mazya <mazur097mazur@gmail.com>
2024-04-08 15:55:34 +0200

    Use extracted on moderator's page

--- src/app/moderator/users/page.tsx
+++ src/app/moderator/users/page.tsx
@@ -1,12 +1,4 @@
-import { db } from "@/drizzle/db";
-import {
-  Application,
-  CommentValue,
-  Image,
-  Review,
-  User,
-} from "@/drizzle/schema";
-import { countDistinct, eq, sql } from "drizzle-orm";
+import { getModeratorPanelUsers } from "@/drizzle/queries/user";
 
 import { UserCell } from "@/components/ui/applicationsTable/cells/userCell";
 import { EtherscanLink } from "@/components/ui/etherscanLink";
@@ -24,30 +16,7 @@ import { ErrorCircleIcon } from "@/components/icons/errorCircleIcon";
 import { ThumbUpIcon } from "@/components/icons/thumbUpIcon";
 
 export default async function UsersPage() {
-  const users = await db
-    .select({
-      id: User.id,
-      name: User.name,
-      ethereumAddress: User.ethereumAddress,
-      createdAt: User.createdAt,
-      image: Image,
-      reviewsCount: countDistinct(Review.commentId),
-      spamCount:
-        sql<number>`count(case when ${CommentValue.value} = 'spam' then 1 end)`.mapWith(
-          Number,
-        ),
-      helpfulCount:
-        sql<number>`count(case when ${CommentValue.value} = 'positive' then 1 end)`.mapWith(
-          Number,
-        ),
-      submissionsCount: countDistinct(Application.id),
-    })
-    .from(User)
-    .leftJoin(Image, eq(Image.id, User.imageId))
-    .leftJoin(Review, eq(User.id, Review.userId))
-    .leftJoin(CommentValue, eq(User.id, CommentValue.userId))
-    .leftJoin(Application, eq(User.id, Application.userId))
-    .groupBy(User.id, Image.id);
+  const users = await getModeratorPanelUsers();
 
   return (
     <div className="flex flex-col gap-4">
