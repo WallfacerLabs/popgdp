3776ab063e0b5609f7fe3e3f981f8c7499c5c2db
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2024-04-08 19:46:11 +0200

    👲 Add verification level to user data (#335)

--- src/config/userPermissions.ts
+++ src/config/userPermissions.ts
@@ -1,6 +1,6 @@
 import { getUserRoles } from "@/drizzle/queries/user";
 
-import { getUserId } from "@/lib/auth";
+import { getUserData } from "@/lib/auth";
 
 export enum UserPermission {
   visitor = 0,
@@ -11,12 +11,12 @@ export enum UserPermission {
 }
 
 export async function getUserPermission(): Promise<UserPermission> {
-  const userId = await getUserId();
-  if (!userId) {
+  const user = await getUserData();
+  if (!user?.id) {
     return UserPermission.visitor;
   }
 
-  const { isReviewer, isModerator } = await getUserRoles(userId);
+  const { isReviewer, isModerator } = await getUserRoles(user.id);
 
   if (isModerator) {
     return UserPermission.moderator;
@@ -26,7 +26,11 @@ export async function getUserPermission(): Promise<UserPermission> {
     return UserPermission.reviewer;
   }
 
-  return UserPermission.orbVerified;
+  if (user?.verificationLevel === "orb") {
+    return UserPermission.orbVerified;
+  }
+
+  return UserPermission.deviceVerified;
 }
 
 export async function userHasRole(permission: UserPermission) {
