0d2a3624c4f9d3ec5378078ef6f8c3875f7846c5
nezouse <stankiewicz1998@gmail.com>
2024-04-10 12:09:41 +0200

    💎 Disable apply for submission without login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import { AccountButton } from "@/components/ui/accountButton";
 import { Button } from "@/components/ui/button";
 import { DevFloatingPanel } from "@/components/ui/devPanel/devPanel";
 import { Separator } from "@/components/ui/separator";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { AssignmentIcon } from "@/components/icons/assignmentIcon";
 import { ProductAnimatedIcon } from "@/components/icons/productAnimatedIcon";
 import { TuneIcon } from "@/components/icons/tuneIcon";
@@ -35,47 +36,49 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AxiomWebVitals />
-      <body
-        className={cn(
-          "flex min-h-screen flex-col items-center bg-background font-sans antialiased",
-          inter.variable,
-        )}
-      >
-        <DevFloatingPanel />
-        <header className="mx-auto grid w-full max-w-[1384px] grid-cols-[292px_1fr_292px] items-center gap-8 p-3">
-          <Button
-            variant="link"
-            className="flex items-center gap-1.5 justify-self-start px-0 font-bold before:bottom-2 before:right-0 before:w-[calc(100%-46px)] before:bg-primary/0"
-            asChild
-          >
-            <Link
-              href={urls.root}
-              className="before:left-[unset] before:right-0"
+      <TooltipProvider delayDuration={100}>
+        <body
+          className={cn(
+            "flex min-h-screen flex-col items-center bg-background font-sans antialiased",
+            inter.variable,
+          )}
+        >
+          <DevFloatingPanel />
+          <header className="mx-auto grid w-full max-w-[1384px] grid-cols-[292px_1fr_292px] items-center gap-8 p-3">
+            <Button
+              variant="link"
+              className="flex items-center gap-1.5 justify-self-start px-0 font-bold before:bottom-2 before:right-0 before:w-[calc(100%-46px)] before:bg-primary/0"
+              asChild
             >
-              <ProductAnimatedIcon
-                className="!h-10 !w-10"
-                aria-label="POPGDP logo"
-              />
-              <h1 className="px-3">POPGDP</h1>
-            </Link>
-          </Button>
+              <Link
+                href={urls.root}
+                className="before:left-[unset] before:right-0"
+              >
+                <ProductAnimatedIcon
+                  className="!h-10 !w-10"
+                  aria-label="POPGDP logo"
+                />
+                <h1 className="px-3">POPGDP</h1>
+              </Link>
+            </Button>
 
-          <Navigation />
+            <Navigation />
 
-          <AccountButton />
-        </header>
+            <AccountButton />
+          </header>
 
-        <main className="mx-auto mt-8 w-full max-w-[1144px] flex-grow px-3">
-          {children}
-        </main>
+          <main className="mx-auto mt-8 w-full max-w-[1144px] flex-grow px-3">
+            {children}
+          </main>
 
-        <footer className="mx-auto mb-6 mt-8 w-full max-w-[1144px] px-3">
-          <Separator className="mb-6 w-full" decorative />
-          <p className="text-center text-xs font-semibold">
-            © 2024 POPGDP. All rights reserved
-          </p>
-        </footer>
-      </body>
+          <footer className="mx-auto mb-6 mt-8 w-full max-w-[1144px] px-3">
+            <Separator className="mb-6 w-full" decorative />
+            <p className="text-center text-xs font-semibold">
+              © 2024 POPGDP. All rights reserved
+            </p>
+          </footer>
+        </body>
+      </TooltipProvider>
     </html>
   );
 }
--- src/app/waves/[waveId]/page.tsx
+++ src/app/waves/[waveId]/page.tsx
@@ -4,11 +4,17 @@ import { urls } from "@/constants/urls";
 import { getWaveWithApplications } from "@/drizzle/queries/waves";
 import { z } from "zod";
 
-import { parseWaveParams } from "@/lib/paramsValidation";
+import { getUserId } from "@/lib/auth";
+import { parseWaveParams, WaveParamsSchema } from "@/lib/paramsValidation";
 import { ApplicationsTable } from "@/components/ui/applicationsTable/applicationsTable";
 import { Button } from "@/components/ui/button";
 import { PageTitle } from "@/components/ui/pageTitle";
 import { TablePagination } from "@/components/ui/pagination/tablePagination";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { TimelinePreview } from "@/components/ui/wavesTimelinePreview/timelinePreview";
 
 import { WaveBanner } from "./waveBanner";
@@ -51,11 +57,7 @@ export default async function Wave({
 
       <div className="mt-6 flex items-center justify-between">
         <PageTitle>Submissions</PageTitle>
-        <Button asChild>
-          <Link href={urls.applications.create({ waveId })}>
-            Apply for Grant
-          </Link>
-        </Button>
+        <ApplyForGrantButton waveId={waveId} />
       </div>
 
       <ApplicationsTable
@@ -68,5 +70,28 @@ export default async function Wave({
         <TablePagination currentPage={page} totalPages={totalPages} />
       )}
     </>
+  );
+}
+
+async function ApplyForGrantButton({ waveId }: WaveParamsSchema) {
+  const userId = await getUserId();
+
+  if (!userId) {
+    return (
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button disabled>Apply for Grant</Button>
+        </TooltipTrigger>
+        <TooltipContent align="end">
+          You must sign in to apply for a grant
+        </TooltipContent>
+      </Tooltip>
+    );
+  }
+
+  return (
+    <Button asChild>
+      <Link href={urls.applications.create({ waveId })}>Apply for Grant</Link>
+    </Button>
   );
 }
