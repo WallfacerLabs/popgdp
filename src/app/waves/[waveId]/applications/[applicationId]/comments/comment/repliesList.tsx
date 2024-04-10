6fc54a784e12aa35ba4b97714d63025a0783a245
mazya <mazur097mazur@gmail.com>
2024-04-10 16:02:12 +0200

    Restyle review replies

--- src/app/waves/[waveId]/applications/[applicationId]/comments/comment/repliesList.tsx
+++ src/app/waves/[waveId]/applications/[applicationId]/comments/comment/repliesList.tsx
@@ -3,6 +3,7 @@
 import { Fragment } from "react";
 
 import { type Comment } from "@/types/Comment";
+import { cn } from "@/lib/cn";
 import {
   Accordion,
   AccordionContent,
@@ -10,7 +11,6 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Separator } from "@/components/ui/separator";
-import { ArrowIcon } from "@/components/icons/arrowIcon";
 
 import { CommentBody } from "./commentBody";
 
@@ -21,25 +21,33 @@ interface RepliesListProps {
 export const RepliesList = ({ replies }: RepliesListProps) => {
   return (
     <Accordion title="Application details" type="multiple">
-      <AccordionItem value="applicationDetails" className="flex flex-col">
-        <AccordionTrigger className="group relative w-full justify-center gap-4 px-4 text-sm text-primary/50 before:h-px before:w-full before:bg-primary/10 after:h-px after:w-full after:bg-primary/10">
-          <div className="flex items-center gap-1">
-            <span className="group-data-[state=open]:hidden">Show</span>
-            <span className="group-data-[state=closed]:hidden">Hide</span>
-            replies
-            <ArrowIcon direction="down" />
-          </div>
-        </AccordionTrigger>
-        <AccordionContent className="pl-10">
+      <AccordionItem
+        value="applicationDetails"
+        className="relative flex flex-col"
+      >
+        <AccordionContent className="mt-4 pl-16">
           <div className="flex flex-col gap-4">
             {replies.map((reply) => (
               <Fragment key={reply.id}>
                 <CommentBody comment={reply} />
-                <Separator className="my-6 last:hidden" />
+                <Separator className="last:hidden" />
               </Fragment>
             ))}
           </div>
         </AccordionContent>
+        <AccordionTrigger
+          className={cn(
+            "group w-full justify-center gap-4 px-7 py-2 text-sm text-primary/50",
+            "before:pointer-events-none before:absolute before:bottom-4 before:left-7 before:h-[calc(100%-1rem)] before:w-1/2 before:rounded-bl-xl before:border-b before:border-l before:border-b-primary/10",
+            "after:pointer-events-none after:absolute after:bottom-4 after:right-4 after:h-[calc(100%-1rem)] after:w-1/2 after:border-b after:border-b-primary/10",
+          )}
+        >
+          <div className="z-10 flex items-center gap-1 bg-background px-2">
+            <span className="group-data-[state=open]:hidden">Show</span>
+            <span className="group-data-[state=closed]:hidden">Hide</span>
+            replies
+          </div>
+        </AccordionTrigger>
       </AccordionItem>
     </Accordion>
   );
