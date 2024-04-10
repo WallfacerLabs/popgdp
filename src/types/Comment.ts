3946751081ca018c00f577727aa7ee3190c04311
mazya <mazur097mazur@gmail.com>
2024-04-10 15:15:33 +0200

    Export types related to comments

--- src/types/Comment.ts
+++ src/types/Comment.ts
@@ -1,3 +1,11 @@
 import { type ApplicationWithComments } from "./Application";
 
 export type Comment = ApplicationWithComments["comments"][number];
+
+export const COMMENT_SECTIONS = {
+  discussion: "Discussion",
+  reviews: "Reviews",
+} as const;
+
+export type CommentSection =
+  (typeof COMMENT_SECTIONS)[keyof typeof COMMENT_SECTIONS];
