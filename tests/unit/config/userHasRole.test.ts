bc67be8c25e2e32b89f27bcb15201409c69b90c4
JustynaBroniszewska <jusbroniszewska@gmail.com>
2024-04-15 15:47:39 +0200

    Run prettier

--- tests/unit/config/userHasRole.test.ts
+++ tests/unit/config/userHasRole.test.ts
@@ -1,11 +1,12 @@
-import { UserPermission, userHasRole } from "@/config/userPermissions";
-import { afterEach, describe, expect, test, it } from "vitest";
-import { clearSession, setUser } from '../../helpers/getSession'
 import { db } from "@/drizzle/db";
 import { Moderator, Reviewer, User } from "@/drizzle/schema";
+import { afterEach, describe, expect, it, test } from "vitest";
 
-describe('userHasRole', () => {
+import { userHasRole, UserPermission } from "@/config/userPermissions";
 
+import { clearSession, setUser } from "../../helpers/getSession";
+
+describe("userHasRole", () => {
   afterEach(async () => {
     await db.delete(Moderator);
     await db.delete(User);
@@ -13,65 +14,65 @@ describe('userHasRole', () => {
     clearSession();
   });
 
-  const ethereumAddress = '0x88009922334455'
-  const id = '0'
+  const ethereumAddress = "0x88009922334455";
+  const id = "0";
 
   async function createModerator() {
-    await db.insert(Moderator).values({ ethereumAddress })
-    await db.insert(User).values({ id, ethereumAddress})
+    await db.insert(Moderator).values({ ethereumAddress });
+    await db.insert(User).values({ id, ethereumAddress });
   }
 
   async function createReviewer() {
-    await db.insert(Reviewer).values({ ethereumAddress })
-    await db.insert(User).values({ id, ethereumAddress})
+    await db.insert(Reviewer).values({ ethereumAddress });
+    await db.insert(User).values({ id, ethereumAddress });
   }
 
   async function createBlocked() {
-    await db.insert(User).values({ id, ethereumAddress, isBlocked: true })
+    await db.insert(User).values({ id, ethereumAddress, isBlocked: true });
   }
 
   async function createOrbVerified() {
-    await db.insert(User).values({ id, ethereumAddress, })
+    await db.insert(User).values({ id, ethereumAddress });
   }
 
-  it('no user', async () => {
+  it("no user", async () => {
     expect(await userHasRole(UserPermission.visitor)).toBe(true);
     expect(await userHasRole(UserPermission.moderator)).toBe(false);
     expect(await userHasRole(UserPermission.blocked)).toBe(false);
     expect(await userHasRole(UserPermission.reviewer)).toBe(false);
     expect(await userHasRole(UserPermission.orbVerified)).toBe(false);
     expect(await userHasRole(UserPermission.deviceVerified)).toBe(false);
-  })
+  });
 
-  it.skip('moderator', async () => {
-    await createModerator()
-    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: 'orb' });
+  it.skip("moderator", async () => {
+    await createModerator();
+    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: "orb" });
     expect(await userHasRole(UserPermission.moderator)).toBe(true);
     expect(await userHasRole(UserPermission.blocked)).toBe(false);
     // expect(await userHasRole(UserPermission.reviewer)).toBe(false);
     // expect(await userHasRole(UserPermission.orbVerified)).toBe(false);
     // expect(await userHasRole(UserPermission.deviceVerified)).toBe(false);
     expect(await userHasRole(UserPermission.visitor)).toBe(false);
-  })
+  });
 
-  it('blocked', async () => {
-    await createBlocked()
-    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: 'orb' });
+  it("blocked", async () => {
+    await createBlocked();
+    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: "orb" });
     expect(await userHasRole(UserPermission.blocked)).toBe(true);
     expect(await userHasRole(UserPermission.visitor)).toBe(true);
-  })
+  });
 
-  it.skip('orb verified', async () => {
+  it.skip("orb verified", async () => {
     // no need for checking in the db? really?
-    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: 'orb' });
+    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: "orb" });
     expect(await userHasRole(UserPermission.orbVerified)).toBe(true);
-  })
+  });
 
-  it('reviewer', async () => {
-    await createReviewer()
-    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: 'orb' });
+  it("reviewer", async () => {
+    await createReviewer();
+    setUser({ sub: `oauth2|worldcoin|${id}`, credentialType: "orb" });
     expect(await userHasRole(UserPermission.reviewer)).toBe(true);
     expect(await userHasRole(UserPermission.moderator)).toBe(false);
     // expect(await userHasRole(UserPermission.blocked)).toBe(false);
-  })
-})
+  });
+});
