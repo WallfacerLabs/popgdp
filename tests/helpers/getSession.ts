7deeeec5a9be0decf95f975101eebcf4908eb7c3
JustynaBroniszewska <jusbroniszewska@gmail.com>
2024-04-15 15:42:49 +0200

    🍯 Setup tests environment

---
+++ tests/helpers/getSession.ts
@@ -0,0 +1,15 @@
+let user = {}
+ 
+export function getSession() {
+  return {
+    user
+  }
+}
+
+export function setUser(newUser: any) {
+  user = newUser;
+}
+
+export function clearSession() {
+  user = {}
+}
