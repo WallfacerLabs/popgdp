921e3683167ace6439451dbb8b2e707e03d43b97
mazya <mazur097mazur@gmail.com>
2024-04-16 14:20:40 +0200

    Add useSubmissionsSortState hook

---
+++ src/hooks/useSubmissionsSortState.ts
@@ -0,0 +1,47 @@
+import { SortBy } from '@/types/Sort';
+import { useSearchState } from "./useSearchState";
+
+const DEFAULT_SORT_BY: SubmissionsListColumn = "name";
+
+export const SUBMISSIONS_LIST_COLUMNS = [
+  "name",
+  "user",
+  "entity",
+  "submissionDate",
+  "budget",
+  "category",
+];
+
+export type SubmissionsListColumn = (typeof SUBMISSIONS_LIST_COLUMNS)[number];
+
+export interface SubmissionsSortBy extends Omit<SortBy, "sortName"> {
+  sortName: SubmissionsListColumn;
+}
+
+export function useSubmissionsSortState() {
+  const { searchParams, updateSearchParams } = useSearchState();
+
+  const handleSortBy = (name: SubmissionsSortBy["sortName"]): void => {
+    const descendingByDefault = name === "tvl" || name === "apy";
+    const sortBy = searchParams.get("sortBy");
+    const previousAsc = searchParams.get("asc") === "true";
+    const asc = sortBy === name ? !previousAsc : !descendingByDefault;
+
+    updateSearchParams(["sortBy", name], ["asc", asc.toString()]);
+  };
+
+  const asc = searchParams.get("asc") === "true";
+  const sortByRaw = searchParams.get("sortBy") ?? "";
+  const sortName = SUBMISSIONS_LIST_COLUMNS.includes(
+    sortByRaw as SubmissionsListColumn,
+  )
+    ? sortByRaw
+    : DEFAULT_SORT_BY;
+
+  const sortBy = {
+    sortName,
+    asc,
+  };
+
+  return { handleSortBy, sortBy };
+}
