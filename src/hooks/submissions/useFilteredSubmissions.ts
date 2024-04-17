f177597010a239b62db41defd473907d682730bc
mazya <mazur097mazur@gmail.com>
2024-04-17 14:50:54 +0200

    Extract filtered submissions hook

---
+++ src/hooks/submissions/useFilteredSubmissions.ts
@@ -0,0 +1,23 @@
+import { Application } from "@/types/Application";
+import { Category } from "@/types/Category";
+
+interface FilteredSubmissionsProps {
+  applications: Application[];
+  category: Category["id"] | undefined;
+  search: string;
+}
+
+export function useFilteredSubmissions({
+  applications,
+  category,
+  search,
+}: FilteredSubmissionsProps) {
+  return applications
+    .filter(
+      (application) =>
+        category === "all" || !category || application.categoryId === category,
+    )
+    .filter((application) =>
+      application.name.toLowerCase().includes(search.toLowerCase()),
+    );
+}
