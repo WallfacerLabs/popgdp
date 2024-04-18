90b5d1a684cdd3b93c796db8ab8a4d0c554c9b8d
mazya <mazur097mazur@gmail.com>
2024-04-18 16:18:57 +0200

    Update interaction styles of select

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -21,7 +21,12 @@ const SelectTrigger = forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "border-input group flex h-10 w-full items-center justify-between rounded-2xl border bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 [&>svg]:h-4 [&>svg]:w-4",
+      "group flex h-10 w-full items-center justify-between rounded-2xl border bg-background px-3 py-2 text-sm transition-colors",
+      "placeholder:text-muted-foreground",
+      "hover:border-primary focus-visible:border-primary focus-visible:outline-none data-[state=open]:border-primary",
+      "disabled:cursor-not-allowed disabled:opacity-50",
+      "[&>span]:line-clamp-1",
+      "[&>svg]:h-4 [&>svg]:w-4",
       className,
     )}
     {...props}
