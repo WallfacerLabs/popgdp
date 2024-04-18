af2e5ed6eb18e26f12c7b7303db60f8a49aabae7
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2024-04-18 15:14:11 +0200

    😭 Extract and test filtered applications util (#412)

---
+++ tests/unit/utils/getFilteredSubmissions.test.ts
@@ -0,0 +1,62 @@
+import { getFilteredSubmissions } from "@/utils/getFilteredSubmissions";
+import { describe, expect, test } from "vitest";
+
+import { Application } from "@/types/Application";
+
+const CATEGORIES_MOCK = {
+  all: "all",
+  first: "first",
+  second: "second",
+};
+
+const APPLICATIONS_MOCK = [
+  { id: "1", name: "First Applications", userId: "user1", categoryId: "first" },
+  {
+    id: "2",
+    name: "Second Applications",
+    userId: "user1",
+    categoryId: "second",
+  },
+  { id: "3", name: "Third Applications", userId: "user2", categoryId: "first" },
+  {
+    id: "4",
+    name: "Fourth Applications",
+    userId: "user3",
+    categoryId: "second",
+  },
+] as Application[];
+
+describe("hooks/useFilteredSubmissions", () => {
+  test("default filters", () => {
+    const filteredApplications = getFilteredSubmissions({
+      applications: APPLICATIONS_MOCK,
+      category: CATEGORIES_MOCK.all,
+      search: "",
+    });
+
+    expect(filteredApplications).toEqual(APPLICATIONS_MOCK);
+  });
+
+  test("search filter", () => {
+    const filteredApplications = getFilteredSubmissions({
+      applications: APPLICATIONS_MOCK,
+      category: CATEGORIES_MOCK.all,
+      search: "First",
+    });
+
+    expect(filteredApplications).toEqual([APPLICATIONS_MOCK[0]]);
+  });
+
+  test("category filter", () => {
+    const filteredApplications = getFilteredSubmissions({
+      applications: APPLICATIONS_MOCK,
+      category: CATEGORIES_MOCK.first,
+      search: "",
+    });
+
+    expect(filteredApplications).toEqual([
+      APPLICATIONS_MOCK[0],
+      APPLICATIONS_MOCK[2],
+    ]);
+  });
+});
