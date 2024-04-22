8cea7510fc325e4178388677ac5e3d2475da828c
mazya <mazur097mazur@gmail.com>
2024-04-22 15:56:57 +0200

    Add util to get filtered users

---
+++ src/utils/getFilteredUsers.ts
@@ -0,0 +1,12 @@
+import { ModeratorPanelUser } from "@/types/User";
+
+interface FilteredUsersProps {
+  users: ModeratorPanelUser[];
+  search: string;
+}
+
+export function getFilteredUsers({ users, search }: FilteredUsersProps) {
+  return users.filter(
+    ({ name }) => name && name.toLowerCase().includes(search.toLowerCase()),
+  );
+}
