300194d03d3b0af4eef6c0a008278be836362870
mazya <mazur097mazur@gmail.com>
2024-04-22 16:12:51 +0200

    Align search user filter

--- src/components/ui/filterPanels/userFiltersPanel.tsx
+++ src/components/ui/filterPanels/userFiltersPanel.tsx
@@ -13,6 +13,7 @@ export function UserFiltersPanel({
       <SearchFilter
         searchPhrase={searchPhrase}
         onSearchPhraseChange={onSearchPhraseChange}
+        className="ml-auto"
       />
     </nav>
   );
