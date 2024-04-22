190b92e6293f42efa4a11b1e760f1e6af07bc57a
mazya <mazur097mazur@gmail.com>
2024-04-22 16:02:51 +0200

    Extract users  table cells

---
+++ src/components/ui/usersTable/cells/userActionsCell.tsx
@@ -0,0 +1,16 @@
+import { ModeratorPanelUser } from "@/types/User";
+import { BlockUserButton } from "@/app/moderator/users/blockUserButton/blockUserButton";
+
+import { TableCell } from "../../table";
+
+interface UserActionsCellProps {
+  user: ModeratorPanelUser;
+}
+
+export const UserActionsCell = ({ user }: UserActionsCellProps) => {
+  return (
+    <TableCell>
+      <BlockUserButton user={user} />
+    </TableCell>
+  );
+};
