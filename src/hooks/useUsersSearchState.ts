39f7c949c99b46e28b6835cd886485c95ff17a79
mazya <mazur097mazur@gmail.com>
2024-04-22 16:23:53 +0200

    Add page to users search state hook

--- src/hooks/useUsersSearchState.ts
+++ src/hooks/useUsersSearchState.ts
@@ -3,12 +3,14 @@ import { useSearchState } from "@/hooks/useSearchState";
 export function useUsersSearchState() {
   const { searchParams, updateSearchParams } = useSearchState();
   const search = searchParams.get("search") ?? "";
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
   function onSearchPhraseChange(value: string) {
     updateSearchParams(["search", value]);
   }
 
   return {
+    page,
     search,
     onSearchPhraseChange,
   };
