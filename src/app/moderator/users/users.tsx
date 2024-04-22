db6f3174e98e3c71202d7fc3cd52cbf356e174f1
mazya <mazur097mazur@gmail.com>
2024-04-22 17:00:02 +0200

    Use users sorting in list

--- src/app/moderator/users/users.tsx
+++ src/app/moderator/users/users.tsx
@@ -4,10 +4,24 @@ import { MAX_ITEMS_PER_PAGE } from "@/constants/pagination";
 import { getFilteredUsers } from "@/utils/getFilteredUsers";
 
 import { ModeratorPanelUser } from "@/types/User";
+import { sortObjectsByKey } from "@/lib/sort";
+import { useSortState } from "@/hooks/useSortState";
 import { useUsersSearchState } from "@/hooks/useUsersSearchState";
 import { UserFiltersPanel } from "@/components/ui/filterPanels/userFiltersPanel";
 import { TablePagination } from "@/components/ui/pagination/tablePagination";
-import { UsersTable } from "@/components/ui/usersTable/usersTable";
+import {
+  ModeratorUsersSortBy,
+  UsersTable,
+} from "@/components/ui/usersTable/usersTable";
+
+export const MODERATOR_USERS_LIST_COLUMNS = [
+  "user",
+  "address",
+  "reviews",
+  "spam",
+  "useful",
+  "submissions",
+];
 
 export const MODERATOR_USERS_LIST_COLUMNS = [
   "user",
@@ -25,13 +39,23 @@ interface UsersProps {
 export function Users({ users }: UsersProps) {
   const { page, search, onSearchPhraseChange } = useUsersSearchState();
 
-  const filteredUsers = getFilteredUsers({
+  const { sortBy, handleSortBy } = useSortState({
+    columns: MODERATOR_USERS_LIST_COLUMNS,
+    defaultDescendingColumns: ["reviews", "spam", "useful", "submissions"],
+  });
+
+  const sortedUsers = getModeratorSortedUsers({
     users,
+    sortBy,
+  });
+
+  const filteredUsers = getFilteredUsers({
+    users: sortedUsers,
     search,
   });
 
-  const applicationsCount = filteredUsers.length;
-  const totalPages = Math.ceil(applicationsCount / MAX_ITEMS_PER_PAGE);
+  const usersCount = filteredUsers.length;
+  const totalPages = Math.ceil(usersCount / MAX_ITEMS_PER_PAGE);
 
   const currentPageUsers = filteredUsers.slice(
     (page - 1) * MAX_ITEMS_PER_PAGE,
@@ -45,11 +69,39 @@ export function Users({ users }: UsersProps) {
         onSearchPhraseChange={onSearchPhraseChange}
       />
 
-      <UsersTable users={currentPageUsers} />
+      <UsersTable
+        users={currentPageUsers}
+        sortBy={sortBy}
+        setSortBy={handleSortBy}
+      />
 
       {totalPages > 1 && (
         <TablePagination currentPage={page} totalPages={totalPages} />
       )}
     </>
   );
+}
+
+function getModeratorSortedUsers({
+  users,
+  sortBy,
+}: {
+  users: ModeratorPanelUser[];
+  sortBy: ModeratorUsersSortBy;
+}) {
+  switch (sortBy.sortName) {
+    case "user":
+    default:
+      return sortObjectsByKey(users, ["name"], sortBy.asc);
+    case "address":
+      return sortObjectsByKey(users, ["ethereumAddress"], sortBy.asc);
+    case "reviews":
+      return sortObjectsByKey(users, ["reviewsCount"], sortBy.asc);
+    case "spam":
+      return sortObjectsByKey(users, ["spamCount"], sortBy.asc);
+    case "useful":
+      return sortObjectsByKey(users, ["helpfulCount"], sortBy.asc);
+    case "submissions":
+      return sortObjectsByKey(users, ["submissionsCount"], sortBy.asc);
+  }
 }
