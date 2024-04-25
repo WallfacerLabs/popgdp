01fa33876a37bf1405ba08c719d2d3cfee96c48e
mazya <mazur097mazur@gmail.com>
2024-04-25 13:56:20 +0200

    Extract moderator panel reviewer type

--- src/types/Reviewer.ts
+++ src/types/Reviewer.ts
@@ -1,3 +1,9 @@
+import { getAllReviewers } from "@/drizzle/queries/user";
+
 export interface Reviewer {
   ethereumAddress: string;
 }
+
+export type ModeratorPanelReviewer = Awaited<
+  ReturnType<typeof getAllReviewers>
+>[number];
