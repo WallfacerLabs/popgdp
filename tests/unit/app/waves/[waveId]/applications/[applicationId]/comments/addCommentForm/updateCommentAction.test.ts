5359d661f645649e8cf96c3d391ff0307a6f59dc
mazya <mazur097mazur@gmail.com>
2024-05-10 14:08:49 +0200

    Update edit comment action test

--- tests/unit/app/waves/[waveId]/applications/[applicationId]/comments/addCommentForm/updateCommentAction.test.ts
+++ tests/unit/app/waves/[waveId]/applications/[applicationId]/comments/addCommentForm/updateCommentAction.test.ts
@@ -23,22 +23,24 @@ const anotherUserId = "anotherUser";
 const waveId = 1;
 const applicationId = "f8e46fab-f2c4-4c46-85ca-9e5cbf716d39";
 const categoryId = "7979fbc1-1a84-4b75-8f7e-bea6f9bf0a99";
-const commentId = "ebf07337-d290-4dc4-b310-361bd24150c5";
+const commentId = "5c8c268c-3a34-43ec-b8b7-58a0cbe9adde";
 
-const defaultApplication = {
-  id: applicationId,
-  userId: anotherUserId,
-  waveId,
-  comments: [],
+const addCommentActionArgs = {
+  application: {
+    id: applicationId,
+    userId: anotherUserId,
+    waveId,
+  },
+  content: "Original comment",
 };
 
-const defaultActionArgs = {
-  applicationId: defaultApplication.id,
+const updateCommentActionArgs = {
+  applicationId,
   commentId,
-  content: "comment",
+  content: "Updated comment",
 };
 
-describe("app/waves/[waveId]/applications/[applicationId]/comments/addCommentForm/addCommentAction", () => {
+describe("app/waves/[waveId]/applications/[applicationId]/comments/addCommentForm/updateCommentAction", () => {
   beforeEach(async () => {
     await createUser(anotherUserId);
     await createWave(waveId);
@@ -64,164 +66,108 @@ describe("app/waves/[waveId]/applications/[applicationId]/comments/addCommentFor
 
   describe("permissions", () => {
     it("visitor", async () => {
-      expect(() => updateCommentAction(defaultActionArgs)).rejects.toThrowError(
-        "You need to be signed in to edit comments",
-      );
+      expect(() =>
+        updateCommentAction(updateCommentActionArgs),
+      ).rejects.toThrowError("You need to be signed in to edit comments");
     });
 
     it("blocked", async () => {
       await createBlocked(userId);
       mockUserSession({ userId, credentialType: "orb" });
 
-      expect(() => updateCommentAction(defaultActionArgs)).rejects.toThrowError(
-        "You are blocked from performing this action",
-      );
+      expect(() =>
+        updateCommentAction(updateCommentActionArgs),
+      ).rejects.toThrowError("You are blocked from performing this action");
     });
 
     it("device verified - other user application", async () => {
       await createUser(userId);
       mockUserSession({ userId, credentialType: "device" });
 
-      expect(() => updateCommentAction(defaultActionArgs)).rejects.toThrowError(
+      expect(() =>
+        updateCommentAction(updateCommentActionArgs),
+      ).rejects.toThrowError(
         "You need to be orb verified to edit comments under other users submissions",
       );
     });
 
     it("device verified - own application", async () => {
-      const updatedCommentContent = "updated comment";
       await createUser(userId);
-      mockUserSession({ userId, credentialType: "device" });
+      mockUserSession({ userId, credentialType: "orb" });
 
-      await updateCommentAction({
-        ...defaultActionArgs,
-        content: updatedCommentContent,
+      await addCommentAction({
+        ...addCommentActionArgs,
+        application: { ...addCommentActionArgs.application, userId },
       });
 
       const comments = await db.query.Comment.findMany();
       expect(comments).toHaveLength(1);
-      expect(comments[0].content).toBe(updatedCommentContent);
+
+      const comment = comments[0];
+      await updateCommentAction({
+        ...updateCommentActionArgs,
+        commentId: comment.id,
+      });
+
+      const updatedComments = await db.query.Comment.findMany();
+      expect(updatedComments[0].content).toBe(updateCommentActionArgs.content);
     });
 
     it("orb verified", async () => {
-      const updatedCommentContent = "updated comment";
       await createUser(userId);
       mockUserSession({ userId, credentialType: "orb" });
 
-      await updateCommentAction({
-        ...defaultActionArgs,
-        content: updatedCommentContent,
-      });
+      await addCommentAction(addCommentActionArgs);
 
       const comments = await db.query.Comment.findMany();
       expect(comments).toHaveLength(1);
-      expect(comments[0].content).toBe(updatedCommentContent);
+
+      const comment = comments[0];
+      await updateCommentAction({
+        ...updateCommentActionArgs,
+        commentId: comment.id,
+      });
+
+      const updatedComments = await db.query.Comment.findMany();
+      expect(updatedComments[0].content).toBe(updateCommentActionArgs.content);
     });
 
     it("reviewer", async () => {
-      const updatedCommentContent = "updated comment";
       await createReviewer(userId);
-      mockUserSession({ userId, credentialType: "device" });
+      mockUserSession({ userId, credentialType: "orb" });
 
-      await updateCommentAction({
-        ...defaultActionArgs,
-        content: updatedCommentContent,
-      });
+      await addCommentAction(addCommentActionArgs);
 
       const comments = await db.query.Comment.findMany();
       expect(comments).toHaveLength(1);
-      expect(comments[0].content).toBe(updatedCommentContent);
+
+      const comment = comments[0];
+      await updateCommentAction({
+        ...updateCommentActionArgs,
+        commentId: comment.id,
+      });
+
+      const updatedComments = await db.query.Comment.findMany();
+      expect(updatedComments[0].content).toBe(updateCommentActionArgs.content);
     });
 
     it("moderator", async () => {
-      const updatedCommentContent = "updated comment";
       await createModerator(userId);
-      mockUserSession({ userId, credentialType: "device" });
+      mockUserSession({ userId, credentialType: "orb" });
 
+      await addCommentAction(addCommentActionArgs);
+
+      const comments = await db.query.Comment.findMany();
+      expect(comments).toHaveLength(1);
+
+      const comment = comments[0];
       await updateCommentAction({
-        ...defaultActionArgs,
-        content: updatedCommentContent,
+        ...updateCommentActionArgs,
+        commentId: comment.id,
       });
 
-      const comments = await db.query.Comment.findMany();
-      expect(comments).toHaveLength(1);
-      expect(comments[0].content).toBe(updatedCommentContent);
+      const updatedComments = await db.query.Comment.findMany();
+      expect(updatedComments[0].content).toBe(updateCommentActionArgs.content);
     });
-  });
-
-  describe("wave stages", () => {
-    it("not open", async () => {
-      await createUser(userId);
-      mockUserSession({ userId, credentialType: "orb" });
-      await updateWaveStage("notOpen");
-
-      expect(() => updateCommentAction(defaultActionArgs)).rejects.toThrowError(
-        "You cannot edit comments in this wave stage",
-      );
-    });
-
-    it("open", async () => {
-      await createUser(userId);
-      mockUserSession({ userId, credentialType: "orb" });
-      await updateWaveStage("open");
-
-      await updateCommentAction(defaultActionArgs);
-
-      const comments = await db.query.Comment.findMany();
-      expect(comments).toHaveLength(1);
-    });
-
-    it("denoising", async () => {
-      await createUser(userId);
-      mockUserSession({ userId, credentialType: "orb" });
-      await updateWaveStage("denoising");
-
-      await updateCommentAction(defaultActionArgs);
-
-      const comments = await db.query.Comment.findMany();
-      expect(comments).toHaveLength(1);
-    });
-
-    it("assesment", async () => {
-      await createUser(userId);
-      mockUserSession({ userId, credentialType: "orb" });
-      await updateWaveStage("assesment");
-
-      await updateCommentAction(defaultActionArgs);
-
-      const comments = await db.query.Comment.findMany();
-      expect(comments).toHaveLength(1);
-    });
-
-    it("close", async () => {
-      await createUser(userId);
-      mockUserSession({ userId, credentialType: "orb" });
-      await updateWaveStage("close");
-
-      expect(() => updateCommentAction(defaultActionArgs)).rejects.toThrowError(
-        "You cannot edit comments in this wave stage",
-      );
-    });
-  });
-
-  it("can edit comment", async () => {
-    await createUser(userId);
-    mockUserSession({ userId, credentialType: "orb" });
-
-    await addCommentAction({
-      application: defaultApplication,
-      content: defaultActionArgs.content,
-    });
-
-    const comments = await db.query.Comment.findMany();
-    expect(comments).toHaveLength(1);
-    expect(comments[0].content).toBe(defaultActionArgs.content);
-
-    const updatedCommentContent = "updated comment";
-
-    await updateCommentAction({
-      ...defaultActionArgs,
-      content: updatedCommentContent,
-    });
-    expect(comments[0].content).toBe(updatedCommentContent);
   });
 });
