672657971e4c48550f77ba822a549b039955467e
mazya <mazur097mazur@gmail.com>
2024-05-10 16:53:12 +0200

    📝 Rename sort util argument

--- src/lib/sort.ts
+++ src/lib/sort.ts
@@ -3,8 +3,8 @@ import { compareObjectsByProperties, ObjectEndpoints } from "./comparison";
 export function sortObjectsByKey<
   T extends object,
   Keys extends ObjectEndpoints<T>,
->(vaults: T[], keys: Keys, ascendingOrder: boolean) {
-  return [...vaults].sort((a, b) =>
+>(array: T[], keys: Keys, ascendingOrder: boolean) {
+  return [...array].sort((a, b) =>
     compareObjectsByProperties(a, b, keys, ascendingOrder),
   );
 }
